ce47322789a5412e2352f28f5ed364796f9a9cc0
Ovidiu Feodorov <ovidiu@novaordis.com>
2004-12-18 05:33:33 +0000

    functional tests - initial check in

---
+++ tests/src/org/jboss/messaging/MessagingTestCase.java
@@ -0,0 +1,49 @@
+/**
+ * JBoss, the OpenSource J2EE WebOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.messaging;
+
+import org.jboss.logging.Logger;
+import org.jboss.logging.XLevel;
+import junit.framework.TestCase;
+
+/**
+ * The base class for messaging tests.
+ *
+ * @author <a href="mailto:adrian@jboss.org">Adrian Brock</a>
+ * @author <a href="mailto:ovidiu@jboss.org">Ovidiu Feodorov</a>
+ * @version <tt>$Revision$</tt>
+ */
+public class MessagingTestCase extends TestCase
+{
+   // Attributes ----------------------------------------------------
+
+   /** The logger */
+   protected Logger log = Logger.getLogger(getClass());
+
+   // Constructors --------------------------------------------------
+
+   public MessagingTestCase(String name)
+   {
+      super(name);
+      org.apache.log4j.Logger jboss = org.apache.log4j.Logger.getLogger("org.jboss");
+      jboss.setLevel(XLevel.ALL);
+   }
+
+   // Public --------------------------------------------------------
+
+   // Protected -----------------------------------------------------
+
+   protected void setUp() throws Exception
+   {
+      log.info("========= Start test: " + getName());
+   }
+
+   protected void tearDown() throws Exception
+   {
+      log.info("========== Stop test: " + getName());
+   }
+}
