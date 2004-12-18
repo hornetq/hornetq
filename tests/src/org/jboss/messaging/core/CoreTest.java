d77302ea81b29fb8754ae102f9cf61e3fc5b61a9
Ovidiu Feodorov <ovidiu@novaordis.com>
2004-12-18 05:45:08 +0000

    functional tests - initial check in

---
+++ tests/src/org/jboss/messaging/core/CoreTest.java
@@ -0,0 +1,33 @@
+/**
+ * JBoss, the OpenSource J2EE WebOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.messaging.core;
+
+import org.jboss.messaging.MessagingTestCase;
+
+/**
+ * @author <a href="mailto:ovidiu@jboss.org">Ovidiu Feodorov</a>
+ * @version <tt>$Revision$</tt>
+ */
+public class CoreTest extends MessagingTestCase
+{
+   // Attributes ----------------------------------------------------
+   
+   // Constructors --------------------------------------------------
+
+   public CoreTest(String name)
+   {
+      super(name);
+   }
+
+   // Public --------------------------------------------------------
+
+   public void testSomething() throws Exception
+   {
+
+   }
+
+}
