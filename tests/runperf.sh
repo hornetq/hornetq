6b7451e612a6f4a42b44fd4b871f296bb34ab9e0
Tim Fox <timvolpe@gmail.com>
2005-11-24 11:35:31 +0000

    More tweaks to scripts

--- tests/runperf.sh
+++ tests/runperf.sh
@@ -8,14 +8,19 @@ ${JAVA_HOME}/bin/java -Dperf.jndiProperties=perf-jndi.properties -Dperf.properti
 ../../thirdparty/apache-log4j/lib/log4j.jar:\
 etc:\
 ../../j2ee/output/lib/jboss-j2ee.jar:\
+../../j2se/output/lib/jboss-j2se.jar:\
 ../../thirdparty/jboss/remoting/lib/jboss-remoting.jar:\
 ../../common/output/lib/jboss-common.jar:\
 ../../thirdparty/jboss/remoting/lib/jboss-serialization.jar:\
-../output/lib/jboss-messaging.jar:\
-/dev/jboss-head/build/output/jboss-5.0.0alpha/client/jbossall-client.jar:\
-../../aop/output/lib/jboss-aop.jar:\
-../src/resources/hsqldb.jar" \
+../src/resources/hsqldb.jar:\
+../../thirdparty/apache-httpclient/lib/commons-httpclient.jar:\
+../../thirdparty/sun-servlet/lib/servlet-api.jar:\
+../../thirdparty/oswego-concurrent/lib/concurrent.jar" \
 org.jboss.test.messaging.jms.perf.framework.PerfRunner
+
+
+
+
 
 
 
--- tests/runslave.sh
+++ tests/runslave.sh
@@ -8,10 +8,11 @@ ${JAVA_HOME}/bin/java -cp \
 ../../thirdparty/apache-log4j/lib/log4j.jar:\
 etc:\
 ../../j2ee/output/lib/jboss-j2ee.jar:\
+../../j2se/output/lib/jboss-j2se.jar:\
 ../../thirdparty/jboss/remoting/lib/jboss-remoting.jar:\
 ../../common/output/lib/jboss-common.jar:\
 ../../thirdparty/jboss/remoting/lib/jboss-serialization.jar:\
-../output/lib/jboss-messaging.jar:\
-/dev/jboss-head/build/output/jboss-5.0.0alpha/client/jbossall-client.jar:\
-../../aop/output/lib/jboss-aop.jar" \
+../../thirdparty/apache-httpclient/lib/commons-httpclient.jar:\
+../../thirdparty/sun-servlet/lib/servlet-api.jar:\
+../../thirdparty/oswego-concurrent/lib/concurrent.jar" \
 org.jboss.test.messaging.jms.perf.framework.Slave $*
