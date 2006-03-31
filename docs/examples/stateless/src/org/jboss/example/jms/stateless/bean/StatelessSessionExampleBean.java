3e7bdeef3c29a44264a13a02eac2d3d4af418ada
Lux Texier <ltexier@redhat.com>
2006-03-31 17:17:09 +0000

    revamped to show how to use finally to use the connection returned by the JMS RA

--- docs/examples/stateless/src/org/jboss/example/jms/stateless/bean/StatelessSessionExampleBean.java
+++ docs/examples/stateless/src/org/jboss/example/jms/stateless/bean/StatelessSessionExampleBean.java
@@ -6,15 +6,7 @@
  */
 package org.jboss.example.jms.stateless.bean;
 
-import javax.jms.TextMessage;
-import javax.jms.Session;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.MessageConsumer;
-import javax.jms.QueueBrowser;
-import javax.jms.Message;
+import javax.jms.*;
 import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
@@ -34,42 +26,204 @@ public class StatelessSessionExampleBean implements SessionBean
 {
 
    private SessionContext ctx;
-   private Connection connection;
 
-   public void ejbCreate()
+    private ConnectionFactory cf = null;
+
+   public void drain(String queueName) throws Exception
    {
+      InitialContext ic = new InitialContext();
+      Queue queue = (Queue)ic.lookup(queueName);
+      ic.close();
+
+      Session session = null;
+      Connection conn = null;
+
       try
       {
-         InitialContext ic = new InitialContext();
-
-         ConnectionFactory cf = (ConnectionFactory)ic.lookup("java:/JmsXA");
-
-         connection = cf.createConnection();
-         connection.start();
-
-         ic.close();
+         conn = getConnection();
+         session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         MessageConsumer consumer = session.createConsumer(queue);
+         Message m = null;
+         do
+         {
+            m = consumer.receiveNoWait();
+         }
+         while(m != null);
       }
-      catch(Exception e)
+      finally
       {
-         e.printStackTrace();
-         throw new EJBException("Initalization failure: " + e.getMessage());
+         if (conn != null)
+         {
+            closeConnection(conn);
+         }
       }
    }
 
-   public void setSessionContext(SessionContext ctx) throws EJBException, RemoteException
+   public void send(String txt, String queueName) throws Exception
+   {
+      InitialContext ic = new InitialContext();
+      Queue queue = (Queue)ic.lookup(queueName);
+      ic.close();
+
+      Session session = null;
+      Connection conn = null;
+
+       try
+      {
+         conn = getConnection();
+         session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         MessageProducer producer = session.createProducer(queue);
+
+         TextMessage tm = session.createTextMessage(txt);
+
+         producer.send(tm);
+         System.out.println("message " + txt + " sent to " + queueName);
+
+      }
+      finally
+      {
+         if (conn != null)
+         {
+            closeConnection(conn);
+         }
+      }
+   }
+
+   public List browse(String queueName) throws Exception
+   {
+      InitialContext ic = new InitialContext();
+      Queue queue = (Queue)ic.lookup(queueName);
+      ic.close();
+
+      Session session = null;
+      Connection conn = null;
+
+      try
+      {
+         conn = getConnection();
+         session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         QueueBrowser browser = session.createBrowser(queue);
+
+         ArrayList list = new ArrayList();
+         for(Enumeration e = browser.getEnumeration(); e.hasMoreElements(); )
+         {
+            list.add(e.nextElement());
+         }
+
+         return list;
+      }
+      finally
+      {
+         if (conn != null)
+         {
+            closeConnection(conn);
+         }
+      }
+   }
+
+   public String receive(String queueName) throws Exception
+   {
+      InitialContext ic = new InitialContext();
+      Queue queue = (Queue)ic.lookup(queueName);
+      ic.close();
+
+      Session session = null;
+      Connection conn = null;
+
+      try
+      {
+         conn = getConnection();
+         session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         MessageConsumer consumer = session.createConsumer(queue);
+
+         System.out.println("blocking to receive message from queue " + queueName + " ...");
+         TextMessage tm = (TextMessage)consumer.receive(5000);
+
+         if (tm == null)
+         {
+            throw new Exception("No message!");
+         }
+
+         System.out.println("Message " + tm.getText() + " received");
+
+         return tm.getText();
+
+      }
+      finally
+      {
+         if (conn != null)
+         {
+            closeConnection(conn);
+         }
+      }
+   }
+
+    public Connection getConnection() throws Exception {
+
+        Connection connection = null;
+
+        try {
+            connection = cf.createConnection();
+            connection.start();
+
+        }catch(Exception e ){
+           if(connection != null)
+               closeConnection(connection);
+           System.out.println("Failed to get connection...exception is " +e);
+           throw e;
+        }
+
+        return connection;
+    }
+
+    public void closeConnection(Connection con) throws Exception {
+
+       try {
+           con.close();
+
+       }catch(JMSException jmse) {
+           System.out.println("Could not close connection " + con +" exception was " +jmse);
+           throw jmse;
+       }
+    }
+
+  public void setSessionContext(SessionContext ctx) throws EJBException, RemoteException
    {
       this.ctx = ctx;
    }
+
+
+
+
+    public void ejbCreate()
+    {
+       try
+       {
+          InitialContext ic = new InitialContext();
+
+          cf = (ConnectionFactory)ic.lookup("java:/JmsXA");
+
+          ic.close();
+       }
+       catch(Exception e)
+       {
+          e.printStackTrace();
+          throw new EJBException("Initalization failure: " + e.getMessage());
+       }
+    }
 
    public void ejbRemove() throws EJBException
    {
       try
       {
-         connection.close();
-      }
+          if(cf != null)
+              cf = null;
+       }
       catch(Exception e)
       {
-         throw new EJBException("Tear down failure", e);
+         throw new EJBException("ejbRemove ", e);
       }
    }
 
@@ -82,148 +236,4 @@ public class StatelessSessionExampleBean implements SessionBean
    }
 
 
-
-
-   public void drain(String queueName) throws Exception
-   {
-      InitialContext ic = new InitialContext();
-      Queue queue = (Queue)ic.lookup(queueName);
-      ic.close();
-
-      Session session = null;
-      try
-      {
-         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-         MessageConsumer consumer = session.createConsumer(queue);
-         Message m = null;
-         do
-         {
-            m = consumer.receiveNoWait();
-         }
-         while(m != null);
-      }
-      finally
-      {
-         if (session != null)
-         {
-            session.close();
-         }
-      }
-   }
-
-
-
-
-   public void send(String txt, String queueName) throws Exception
-   {
-      InitialContext ic = new InitialContext();
-      Queue queue = (Queue)ic.lookup(queueName);
-      ic.close();
-
-      Session session = null;
-      try
-      {
-         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-
-
-         MessageProducer producer = session.createProducer(queue);
-
-
-
-         TextMessage tm = session.createTextMessage(txt);
-
-
-
-         producer.send(tm);
-         System.out.println("message " + txt + " sent to " + queueName);
-
-      }
-      finally
-      {
-         if (session != null)
-         {
-            session.close();
-         }
-      }
-   }
-
-
-
-   public List browse(String queueName) throws Exception
-   {
-      InitialContext ic = new InitialContext();
-      Queue queue = (Queue)ic.lookup(queueName);
-      ic.close();
-
-      Session session = null;
-      try
-      {
-         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-
-
-         QueueBrowser browser = session.createBrowser(queue);
-
-
-         ArrayList list = new ArrayList();
-         for(Enumeration e = browser.getEnumeration(); e.hasMoreElements(); )
-         {
-            list.add(e.nextElement());
-         }
-
-
-         return list;
-      }
-      finally
-      {
-         if (session != null)
-         {
-            session.close();
-         }
-      }
-   }
-
-
-
-   public String receive(String queueName) throws Exception
-   {
-      InitialContext ic = new InitialContext();
-      Queue queue = (Queue)ic.lookup(queueName);
-      ic.close();
-
-      Session session = null;
-      try
-      {
-         session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-
-
-         MessageConsumer consumer = session.createConsumer(queue);
-
-
-
-         System.out.println("blocking to receive message from queue " + queueName + " ...");
-         TextMessage tm = (TextMessage)consumer.receive(5000);
-
-         if (tm == null)
-         {
-            throw new Exception("No message!");
-         }
-
-
-         System.out.println("Message " + tm.getText() + " received");
-
-
-         return tm.getText();
-      }
-
-      finally
-      {
-         if (session != null)
-         {
-            session.close();
-         }
-      }
-   }
 }
