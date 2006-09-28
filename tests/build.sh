bb413ca7b28502ec863c884c2b542e0c536336a9
Ovidiu Feodorov <ovidiu@novaordis.com>
2006-09-28 20:53:03 +0000

    increased heap for HTML report processing

--- tests/build.sh
+++ tests/build.sh
@@ -4,5 +4,6 @@
 reldir=`dirname $0`
 
 #export ANT_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_shmem,server=y,suspend=y,address=ant"
+export ANT_OPTS=-Xmx1024m
 $ANT_HOME/bin/ant -lib $reldir/../thirdparty/junit/lib/junit.jar "$@"
 
