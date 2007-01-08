92282f0212744ad38c3e1ac4883fc985e9ebae70
Clebert Suconic <clebert.suconic@gmail.com>
2007-01-08 20:41:56 +0000

    Fixing IDs and Releases

--- tests/src/org/jboss/test/messaging/core/plugin/base/PostOfficeTestBase.java
+++ tests/src/org/jboss/test/messaging/core/plugin/base/PostOfficeTestBase.java
@@ -53,7 +53,7 @@ import org.jboss.test.messaging.util.CoreMessageFactory;
  * A PostOfficeTestBase
  *
  * @author <a href="mailto:tim.fox@jboss.com">Tim Fox</a>
- * @version <tt>$Revision: 1.1 $</tt>
+ * @version <tt>$Revision$</tt>
  *
  * $Id$
  *
