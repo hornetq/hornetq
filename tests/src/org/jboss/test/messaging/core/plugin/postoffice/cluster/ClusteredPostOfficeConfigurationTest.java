29fe25d6f4f9d5e3ce31c410021c3466cf03d99e
Sergey Koshcheyev <sergey.p.k@gmail.com>
2007-05-30 17:13:49 +0000

    Added license header and Javadoc

--- tests/src/org/jboss/test/messaging/core/plugin/postoffice/cluster/ClusteredPostOfficeConfigurationTest.java
+++ tests/src/org/jboss/test/messaging/core/plugin/postoffice/cluster/ClusteredPostOfficeConfigurationTest.java
@@ -1,3 +1,24 @@
+/*
+  * JBoss, Home of Professional Open Source
+  * Copyright 2005, JBoss Inc., and individual contributors as indicated
+  * by the @authors tag. See the copyright.txt in the distribution for a
+  * full listing of individual contributors.
+  *
+  * This is free software; you can redistribute it and/or modify it
+  * under the terms of the GNU Lesser General Public License as
+  * published by the Free Software Foundation; either version 2.1 of
+  * the License, or (at your option) any later version.
+  *
+  * This software is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this software; if not, write to the Free
+  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+  */
 package org.jboss.test.messaging.core.plugin.postoffice.cluster;
 
 import java.lang.reflect.Field;
@@ -12,9 +33,16 @@ import org.jgroups.JChannel;
 import org.jgroups.protocols.UDP;
 import org.jgroups.stack.Protocol;
 
-// This test assumes that bind_addr is not set in the clustered-*-persistence.xml
-// configuration file!
-// TODO this test actually tests JGroups rather than Messaging
+/**
+ * This test assumes that bind_addr is not set in the clustered-*-persistence.xml
+ * configuration file!
+ * TODO this test actually tests JGroups rather than Messaging
+ *
+ * @author <a href="mailto:sergey.koshcheyev@jboss.com">Sergey Koshcheyev</a>
+ * @version <tt>$Revision$</tt>
+ * 
+ * $Id$
+ */
 public class ClusteredPostOfficeConfigurationTest extends PostOfficeTestBase
 {
    public ClusteredPostOfficeConfigurationTest(String name)
