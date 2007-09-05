3fc87513b08f21f5392a1f2be479d724678f6c58
Clebert Suconic <clebert.suconic@gmail.com>
2007-09-05 14:14:18 +0000

    Changing timing on MessageConsumerTest

--- tests/src/org/jboss/test/messaging/jms/MessageConsumerTest.java
+++ tests/src/org/jboss/test/messaging/jms/MessageConsumerTest.java
@@ -2031,7 +2031,7 @@ public class MessageConsumerTest extends JMSTestCase
 	
 	      for (int i = 0; i < count; i++)
 	      {
-	         Message m = topicConsumer.receive(1500);
+	         Message m = topicConsumer.receive(3000);
 	         assertNotNull(m);
 	      }
 	      
