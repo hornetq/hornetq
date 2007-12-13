1570342cfe07ce7e48723fdfa56a7dfbf052f7f9
Jeff Mesnil <jmesnil@gmail.com>
2007-12-13 10:18:56 +0000

    http://jira.jboss.org/jira/browse/JBMESSAGING-1195 - Optimize in-vm communication * related tests cleanup

--- tests/src/org/jboss/messaging/core/remoting/impl/mina/integration/test/MinaClientTest.java
+++ tests/src/org/jboss/messaging/core/remoting/impl/mina/integration/test/MinaClientTest.java
@@ -6,30 +6,25 @@
  */
 package org.jboss.messaging.core.remoting.impl.mina.integration.test;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.jboss.messaging.core.remoting.TransportType.TCP;
-
-import java.io.IOException;
-import java.util.List;
+import static org.jboss.messaging.core.remoting.impl.mina.integration.test.TestSupport.PORT;
 
 import org.jboss.messaging.core.remoting.Client;
-import org.jboss.messaging.core.remoting.PacketDispatcher;
-import org.jboss.messaging.core.remoting.impl.mina.integration.test.ReversePacketHandler;
-import org.jboss.messaging.core.remoting.impl.mina.integration.test.TestSupport;
-import org.jboss.messaging.core.remoting.integration.MinaConnector;
-import org.jboss.messaging.core.remoting.test.unit.TestPacketHandler;
-import org.jboss.messaging.core.remoting.wireformat.AbstractPacket;
-import org.jboss.messaging.core.remoting.wireformat.TextPacket;
+import org.jboss.messaging.core.remoting.TransportType;
+import org.jboss.messaging.core.remoting.impl.ClientTestBase;
+import org.jboss.messaging.core.remoting.impl.mina.MinaConnector;
+import org.jboss.messaging.core.remoting.impl.mina.MinaService;
 
 /**
- * @author <a href="mailto:jmesnil@redhat.com">Jeff Mesnil</a>.
- * 
+ * @author <a href="mailto:jmesnil@redhat.com">Jeff Mesnil</a>
+ *
  * @version <tt>$Revision$</tt>
+ *
  */
-public class MinaClientTest extends TestSupport
+public class MinaClientTest extends ClientTestBase
 {
-   private ReversePacketHandler serverPacketHandler;
+
+   private MinaService service;
 
    // Constants -----------------------------------------------------
 
@@ -41,133 +36,38 @@ public class MinaClientTest extends TestSupport
 
    // Public --------------------------------------------------------
 
-   public void testConnected() throws Exception
-   {
-      Client client = new Client(new MinaConnector());
-
-      assertFalse(client.isConnected());
-
-      client.connect("localhost", PORT, TCP);
-      assertTrue(client.isConnected());
-
-      assertTrue(client.disconnect());
-      assertFalse(client.isConnected());
-      assertFalse(client.disconnect());
-   }
-      
-   public void testSendOneWay() throws Exception
-   {
-      serverPacketHandler.expectMessage(1);
-
-      TextPacket packet = new TextPacket("testSendOneWay");
-      packet.setVersion((byte) 1);
-      packet.setTargetID(serverPacketHandler.getID());
-      client.sendOneWay(packet);
-
-      assertTrue(serverPacketHandler.await(2, SECONDS));
-
-      List<TextPacket> messages = serverPacketHandler.getPackets();
-      assertEquals(1, messages.size());
-      String response = ((TextPacket) messages.get(0)).getText();
-      assertEquals(packet.getText(), response);
-   }
-
-   public void testSendManyOneWay() throws Exception
-   {
-      serverPacketHandler.expectMessage(MANY_MESSAGES);
-
-      TextPacket[] packets = new TextPacket[MANY_MESSAGES];
-      for (int i = 0; i < MANY_MESSAGES; i++)
-      {
-         packets[i] = new TextPacket("testSendManyOneWay " + i);
-         packets[i].setVersion((byte) 1);
-         packets[i].setTargetID(serverPacketHandler.getID());
-         client.sendOneWay(packets[i]);
-      }
-
-      assertTrue(serverPacketHandler.await(10, SECONDS));
-
-      List<TextPacket> receivedPackets = serverPacketHandler.getPackets();
-      assertEquals(MANY_MESSAGES, receivedPackets.size());
-      for (int i = 0; i < MANY_MESSAGES; i++)
-      {
-         TextPacket receivedPacket = (TextPacket) receivedPackets.get(i);
-         assertEquals(packets[i].getText(), receivedPacket.getText());
-      }
-   }
-
-   public void testSendOneWayWithCallbackHandler() throws Exception
-   {
-      TestPacketHandler callbackHandler = new TestPacketHandler();
-      callbackHandler.expectMessage(1);
-
-      PacketDispatcher.client.register(callbackHandler);
-
-      TextPacket packet = new TextPacket("testSendOneWayWithCallbackHandler");
-      packet.setVersion((byte) 1);
-      packet.setTargetID(serverPacketHandler.getID());
-      packet.setCallbackID(callbackHandler.getID());
-
-      client.sendOneWay(packet);
-
-      assertTrue(callbackHandler.await(5, SECONDS));
-
-      assertEquals(1, callbackHandler.getPackets().size());
-      String response = callbackHandler.getPackets().get(0).getText();
-      assertEquals(reverse(packet.getText()), response);
-   }
-
-   public void testSendBlocking() throws Exception
-   {
-      TextPacket request = new TextPacket("testSendBlocking");
-      request.setVersion((byte) 1);
-      request.setTargetID(serverPacketHandler.getID());
-
-      AbstractPacket receivedPacket = client.sendBlocking(request);
-
-      assertNotNull(receivedPacket);
-      assertTrue(receivedPacket instanceof TextPacket);
-      TextPacket response = (TextPacket) receivedPacket;
-      assertEquals(reverse(request.getText()), response.getText());
-   }
-
-   public void testSendBlockingWithTimeout() throws Exception
-   {
-      client.setBlockingRequestTimeout(500, MILLISECONDS);
-      serverPacketHandler.setSleepTime(1000, MILLISECONDS);
-
-      AbstractPacket packet = new TextPacket("testSendBlockingWithTimeout");
-      packet.setTargetID(serverPacketHandler.getID());
-      
-      packet.setVersion((byte) 1);
-
-      try
-      {
-         client.sendBlocking(packet);
-         fail("a IOException should be thrown");
-      } catch (IOException e)
-      {
-      }
-   }
-
-   // TestCase implementation ---------------------------------------
-
+   // ClientTestBase overrides --------------------------------------
+   
    @Override
-   protected void setUp() throws Exception
+   protected Client createClient() throws Exception
    {
-      startServer(TestSupport.PORT, TRANSPORT);
-      startClient(TestSupport.PORT, TRANSPORT);
-
-      serverPacketHandler = new ReversePacketHandler();
-      PacketDispatcher.server.register(serverPacketHandler);
+      return new Client(new MinaConnector());
    }
 
    @Override
-   protected void tearDown() throws Exception
+   protected TransportType getTransport()
    {
-      PacketDispatcher.server.unregister(serverPacketHandler.getID());
-
-      client.disconnect();
-      stopServer();
+      return TCP;
    }
+
+   @Override
+   protected void startServer() throws Exception
+   {
+      service = new MinaService("localhost", PORT);
+      service.start();
+   }
+
+   @Override
+   protected void stopServer()
+   {
+      service.stop();
+   }
+   
+   // Package protected ---------------------------------------------
+
+   // Protected -----------------------------------------------------
+
+   // Private -------------------------------------------------------
+
+   // Inner classes -------------------------------------------------
 }
--- tests/src/org/jboss/messaging/core/remoting/impl/mina/integration/test/TestSupport.java
+++ tests/src/org/jboss/messaging/core/remoting/impl/mina/integration/test/TestSupport.java
@@ -6,48 +6,23 @@
  */
 package org.jboss.messaging.core.remoting.impl.mina.integration.test;
 
-import static org.jboss.messaging.core.remoting.TransportType.TCP;
-import junit.framework.TestCase;
-
-import org.jboss.messaging.core.remoting.Client;
-import org.jboss.messaging.core.remoting.TransportType;
-import org.jboss.messaging.core.remoting.impl.mina.MinaConnector;
-import org.jboss.messaging.core.remoting.impl.mina.MinaService;
 
 /**
  * @author <a href="mailto:jmesnil@redhat.com">Jeff Mesnil</a>.
  * 
  * @version <tt>$Revision$</tt>
  */
-public abstract class TestSupport extends TestCase
+public abstract class TestSupport
 {
    // Constants -----------------------------------------------------
 
    public static final int MANY_MESSAGES = 500;
 
-   /**
-    * Configurable by system property <code>transport.type</code>, default is
-    * TCP
-    */
-   public final static TransportType TRANSPORT;
-
    // Attributes ----------------------------------------------------
-
-   protected Client client;
-
-   private MinaService service;
 
    public static final int PORT = 9090;
 
    // Static --------------------------------------------------------
-
-   static
-   {
-      String transportType = System.getProperty("transport.type", TCP
-            .toString());
-      TRANSPORT = TransportType.valueOf(transportType);
-      info("Default transport is " + TRANSPORT);
-   }
 
    public static String reverse(String text)
    {
@@ -64,45 +39,11 @@ public abstract class TestSupport extends TestCase
 
    // Public --------------------------------------------------------
 
-   protected void startServer(int port, TransportType transport) throws Exception
-   {
-      startServer(port, transport, false);
-   }
-
-   protected void startServer(int port, TransportType transport, boolean useSSL)
-         throws Exception
-   {
-      service = new MinaService("localhost", port);
-      service.start();
-   }
-   
-   protected void stopServer()
-   {
-      service.stop();
-   }
-
-   protected void startClient(int port, TransportType transport) throws Exception
-   {
-      startClient(port, transport, false);
-   }
-
-   protected void startClient(int port, TransportType transport, boolean useSSL)
-         throws Exception
-   {
-      client = new Client(new MinaConnector());
-      client.connect("localhost", port, transport, useSSL);
-   }
-
    // Package protected ---------------------------------------------
 
    // Protected -----------------------------------------------------
 
    // Private -------------------------------------------------------
-
-   private static void info(String info)
-   {
-      System.out.format("### %-50s ###\n", info);
-   }
 
    // Inner classes -------------------------------------------------
 }
