29e800fa4f9e6af54cc53e88055ecfac2eb94674
Clebert Suconic <clebert.suconic@gmail.com>
2008-01-30 17:45:49 +0000

    JBMESSAGING-1225 - Interceptors cleanup (Adding missing class)

---
+++ src/main/org/jboss/messaging/core/remoting/wireformat/Packet.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+package org.jboss.messaging.core.remoting.wireformat;
+
+public interface Packet
+{
+   // Public --------------------------------------------------------
+
+   void setCorrelationID(long correlationID);
+
+   long getCorrelationID();
+
+   PacketType getType();
+
+   String getTargetID();
+
+   void setTargetID(String targetID);
+
+   void setCallbackID(String callbackID);
+
+   String getCallbackID();
+
+   void setOneWay(boolean oneWay);
+
+   boolean isOneWay();
+   
+   void normalize(Packet other);
+
+   /**
+    * An AbstractPacket is a request if it has a target ID and a correlation ID
+    */
+   public boolean isRequest();
+
+
+}
