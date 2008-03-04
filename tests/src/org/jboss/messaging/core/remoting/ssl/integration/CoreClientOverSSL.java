54a00588d68acb9de6721f4f2fa1743185f40ccc
Jeff Mesnil <jmesnil@gmail.com>
2008-03-04 16:54:35 +0000

    * fixed calls to ClientConnection.createSession() to comply to ackBatchSize assertion

--- tests/src/org/jboss/messaging/core/remoting/ssl/integration/CoreClientOverSSL.java
+++ tests/src/org/jboss/messaging/core/remoting/ssl/integration/CoreClientOverSSL.java
@@ -82,7 +82,7 @@ public class CoreClientOverSSL
          MessagingServer server = new MessagingServerImpl();
          ClientConnectionFactory cf = new ClientConnectionFactoryImpl(0, remotingConf, server.getVersion());
          ClientConnection conn = cf.createConnection(null, null);
-         ClientSession session = conn.createClientSession(false, true, true, 0, false, false);
+         ClientSession session = conn.createClientSession(false, true, true, -1, false, false);
          ClientProducer producer = session.createProducer(QUEUE);
 
          MessageImpl message = new MessageImpl(JBossTextMessage.TYPE, false, 0,
--- tests/src/org/jboss/messaging/core/remoting/ssl/integration/CoreClientOverSSLTest.java
+++ tests/src/org/jboss/messaging/core/remoting/ssl/integration/CoreClientOverSSLTest.java
@@ -130,7 +130,7 @@ public class CoreClientOverSSLTest extends TestCase
 
       ClientConnectionFactory cf = new ClientConnectionFactoryImpl(0, remotingConf, server.getVersion());
       connection = cf.createConnection(null, null);
-      ClientSession session = connection.createClientSession(false, true, true, 0, false, false);
+      ClientSession session = connection.createClientSession(false, true, true, -1, false, false);
       session.createQueue(QUEUE, QUEUE, null, false, false);
       consumer = session.createConsumer(QUEUE, null, false, false, true);
       connection.start();
