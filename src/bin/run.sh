ac62a11328885390638e536d3068dbbf2358c8e8
Andy Taylor <andy.taylor@jboss.org>
2008-05-08 09:38:50 +0000

    added aio libs to standalone distro

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -1,7 +1,7 @@
 export JBM_HOME=..
 export CLASSPATH=$JBM_HOME/config/
-export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin"
-#export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=."
+#export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 for i in `ls $JBM_HOME/lib/*.jar`; do
 	CLASSPATH=$i:$CLASSPATH
 done
