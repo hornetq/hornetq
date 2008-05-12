87722401b738f947156a4220d82621339dfe2cd2
Andy Taylor <andy.taylor@jboss.org>
2008-05-12 12:12:45 +0000

    added windows batch file

---
+++ src/bin/run.bat
@@ -0,0 +1,17 @@
+@ echo off
+setlocal ENABLEDELAYEDEXPANSION
+set JBM_HOME=..
+set CLASSPATH=%JBM_HOME%\config\
+set JVM_ARGS=-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=.
+REM export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+for /R ..\lib %%A in (*.jar) do (	
+SET CLASSPATH=!CLASSPATH!;%%A
+)
+
+echo ***********************************************************************************
+echo "java %JVM_ARGS% -classpath %CLASSPATH% org.jboss.jms.server.microcontainer.JBMBootstrapServer jbm-standalone-beans.xml"
+echo ***********************************************************************************
+java %JVM_ARGS% -classpath %CLASSPATH% org.jboss.messaging.microcontainer.JBMBootstrapServer jbm-standalone-beans.xml
+
+
+
