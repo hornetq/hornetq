7f6256f0c861154b3ae7301f2420e85aff7f4ed8
Tim Fox <timvolpe@gmail.com>
2008-05-14 12:25:29 +0000

    A few tweaks

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -1,6 +1,6 @@
 export JBM_HOME=..
 export CLASSPATH=$JBM_HOME/config/
-export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=."
+export JVM_ARGS="-XX:+UseParallelGC -Xms512M -Xmx1024M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=."
 #export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 for i in `ls $JBM_HOME/lib/*.jar`; do
 	CLASSPATH=$i:$CLASSPATH
