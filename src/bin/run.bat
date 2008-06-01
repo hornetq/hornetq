6614fc2a16ec35cd5be38b5ddca5b1cd0ec7a2a6
Tim Fox <timvolpe@gmail.com>
2008-06-01 16:18:32 +0000

    Few tweaks

--- src/bin/run.bat
+++ src/bin/run.bat
@@ -2,7 +2,7 @@
 setlocal ENABLEDELAYEDEXPANSION
 set JBM_HOME=..
 set CLASSPATH=%JBM_HOME%\config\
-set JVM_ARGS=-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=.
+set JVM_ARGS=-XX:+UseParallelGC -Xms512M -Xmx1024M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=.
 REM export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 for /R ..\lib %%A in (*.jar) do (	
 SET CLASSPATH=!CLASSPATH!;%%A
