2c1cd6fbf0c0d4ae89414468bacbc5f949041d6c
Andy Taylor <andy.taylor@jboss.org>
2008-11-24 10:29:19 +0000

    fixed distro target in build

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -1,11 +1,11 @@
 export JBM_HOME=..
 export CLASSPATH=$JBM_HOME/config/
-export JVM_ARGS="-XX:+UseParallelGC -Xms512M -Xmx1024M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=."
-#export JVM_ARGS="-Xmx512M -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.core.logging.JBMLoggerPlugin -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+export JVM_ARGS="-XX:+UseParallelGC -Xms512M -Xmx1024M -Djava.util.logging.config.file=$JBM_HOME/cdconfig/logging.properties -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.integration.logging.JBMLoggerPlugin -Djava.library.path=."
+#export JVM_ARGS="-Xmx512M -Djava.util.logging.config.file=$JBM_HOME/cdconfig/logging.properties -Dorg.jboss.logging.Logger.pluginClass=org.jboss.messaging.integration.logging.JBMLoggerPlugin -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 for i in `ls $JBM_HOME/lib/*.jar`; do
 	CLASSPATH=$i:$CLASSPATH
 done
 echo ***********************************************************************************
-echo "java $JVM_ARGS -classpath $CLASSPATH org.jboss.jms.server.microcontainer.JBMBootstrapServer jbm-standalone-beans.xml"
+echo "java $JVM_ARGS -classpath $CLASSPATH org.jboss.messaging.integration.bootstrap.JBMBootstrapServer jbm-standalone-beans.xml"
 echo ***********************************************************************************
-java $JVM_ARGS -classpath $CLASSPATH -Dcom.sun.management.jmxremote org.jboss.messaging.microcontainer.JBMBootstrapServer jbm-standalone-beans.xml
+java $JVM_ARGS -classpath $CLASSPATH -Dcom.sun.management.jmxremote org.jboss.messaging.integration.bootstrap.JBMBootstrapServer jbm-standalone-beans.xml
