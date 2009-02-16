9175b8331a01b1dcce1287638784738fc3c6b6a4
Jeff Mesnil <jmesnil@gmail.com>
2009-02-16 16:02:22 +0000

    JBMESSAGING-1499: Setup JORAM JMS compliance tests to run against JBM 2.0
    * use InVM transport since the server and the clients are in the same VM
    * JNDI lookups are done using a regular JNDI server though

--- tests/joram-tests/src/org/jboss/test/jms/JBossMessagingAdmin.java
+++ tests/joram-tests/src/org/jboss/test/jms/JBossMessagingAdmin.java
@@ -58,11 +58,11 @@ import org.jboss.messaging.core.config.Configuration;
 import org.jboss.messaging.core.config.TransportConfiguration;
 import org.jboss.messaging.core.config.impl.ConfigurationImpl;
 import org.jboss.messaging.core.management.ObjectNames;
+import org.jboss.messaging.core.remoting.impl.invm.InVMAcceptorFactory;
+import org.jboss.messaging.core.remoting.impl.invm.InVMConnectorFactory;
 import org.jboss.messaging.core.security.impl.SecurityStoreImpl;
 import org.jboss.messaging.core.server.Messaging;
 import org.jboss.messaging.core.server.impl.MessagingServiceImpl;
-import org.jboss.messaging.integration.transports.netty.NettyAcceptorFactory;
-import org.jboss.messaging.integration.transports.netty.NettyConnectorFactory;
 import org.jboss.messaging.jms.server.impl.JMSServerManagerImpl;
 import org.jboss.messaging.util.SimpleString;
 import org.jnp.server.Main;
@@ -97,7 +97,7 @@ public class JBossMessagingAdmin implements Admin
    
    public void start() throws Exception
    {
-      ClientSessionFactoryImpl sf = new ClientSessionFactoryImpl(new TransportConfiguration(NettyConnectorFactory.class.getName()));
+      ClientSessionFactoryImpl sf = new ClientSessionFactoryImpl(new TransportConfiguration(InVMConnectorFactory.class.getName()));
       clientSession = sf.createSession(SecurityStoreImpl.CLUSTER_ADMIN_USER,
                                        ConfigurationImpl.DEFAULT_MANAGEMENT_CLUSTER_PASSWORD,
                                        false,
@@ -121,7 +121,7 @@ public class JBossMessagingAdmin implements Admin
          invokeSyncOperation(ObjectNames.getJMSServerObjectName(),
                              "createSimpleConnectionFactory",
                              name,
-                             NettyConnectorFactory.class.getName(),
+                             InVMConnectorFactory.class.getName(),
                              DEFAULT_CONNECTION_LOAD_BALANCING_POLICY_CLASS_NAME,
                              DEFAULT_PING_PERIOD,
                              DEFAULT_CONNECTION_TTL,
@@ -259,7 +259,7 @@ public class JBossMessagingAdmin implements Admin
    public void startEmbeddedServer() throws Exception
    {
       Configuration conf = new ConfigurationImpl();
-      conf.getAcceptorConfigurations().add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
+      conf.getAcceptorConfigurations().add(new TransportConfiguration(InVMAcceptorFactory.class.getName()));
       conf.setSecurityEnabled(false);
       embeddedServer = Messaging.newNullStorageMessagingService(conf);
       
