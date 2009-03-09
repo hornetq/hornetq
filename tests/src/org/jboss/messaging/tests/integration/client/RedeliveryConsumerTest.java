31e5dcca337f4e592442ec9de6e40b93d1e03eac
Andy Taylor <andy.taylor@jboss.org>
2009-03-09 13:24:11 +0000

    moved tests

--- tests/src/org/jboss/messaging/tests/integration/client/RedeliveryConsumerTest.java
+++ tests/src/org/jboss/messaging/tests/integration/client/RedeliveryConsumerTest.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.jboss.messaging.tests.integration.consumer;
+package org.jboss.messaging.tests.integration.client;
 
 import org.jboss.messaging.core.client.ClientConsumer;
 import org.jboss.messaging.core.client.ClientMessage;
