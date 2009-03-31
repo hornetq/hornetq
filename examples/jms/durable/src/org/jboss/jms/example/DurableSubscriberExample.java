12061512061f4c563ecf9e2d71194d7070e187b6
Tim Fox <timvolpe@gmail.com>
2009-03-31 10:53:10 +0000

    more tweaks to example

--- examples/jms/durable/src/org/jboss/jms/example/DurableSubscriberExample.java
+++ examples/jms/durable/src/org/jboss/jms/example/DurableSubscriberExample.java
@@ -47,72 +47,72 @@ public class DurableSubscriberExample extends JMSExample
       Connection connection = null;
       try
       {
-         //Step 1. Create an initial context to perform the JNDI lookup.
+         // Step 1. Create an initial context to perform the JNDI lookup.
          InitialContext initialContext = getContext();
-         
-         //Step 2. Look-up the JMS topic
-         Topic topic = (Topic) initialContext.lookup("/topic/exampleTopic");
-         
-         //Step 3. Look-up the JMS connection factory
-         ConnectionFactory cf = (ConnectionFactory) initialContext.lookup("/ConnectionFactory");
-         
-         //Step 4. Create a JMS connection
+
+         // Step 2. Look-up the JMS topic
+         Topic topic = (Topic)initialContext.lookup("/topic/exampleTopic");
+
+         // Step 3. Look-up the JMS connection factory
+         ConnectionFactory cf = (ConnectionFactory)initialContext.lookup("/ConnectionFactory");
+
+         // Step 4. Create a JMS connection
          connection = cf.createConnection();
-         
-         //Step 5. Set the client-id on the connection
+
+         // Step 5. Set the client-id on the connection
          connection.setClientID("durable-client");
-         
-         //Step 6. Start the connection
+
+         // Step 6. Start the connection
          connection.start();
-         
-         //Step 7. Create a JMS session
+
+         // Step 7. Create a JMS session
          Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-         
-         //Step 8. Create a JMS message producer
+
+         // Step 8. Create a JMS message producer
          MessageProducer messageProducer = session.createProducer(topic);
 
-         //Step 9. Create the subscription and the subscriber.
+         // Step 9. Create the subscription and the subscriber.
          TopicSubscriber subscriber = session.createDurableSubscriber(topic, "subscriber-1");
-         
-         //Step 10. Create a text message
+
+         // Step 10. Create a text message
          TextMessage message1 = session.createTextMessage("This is a text message 1");
-         
-         //Step 11. Send the text message to the topic
+
+         // Step 11. Send the text message to the topic
          messageProducer.send(message1);
-         
+
          System.out.println("Sent message: " + message1.getText());
-         
-         //Step 12. Consume the message from the durable subscription
-                           
+
+         // Step 12. Consume the message from the durable subscription
+
          TextMessage messageReceived = (TextMessage)subscriber.receive();
-         
+
          System.out.println("Received message: " + messageReceived.getText());
-         
-         //Step 13. Create and send another message
-         
+
+         // Step 13. Create and send another message
+
          TextMessage message2 = session.createTextMessage("This is a text message 2");
-         
+
          messageProducer.send(message2);
-         
+
          System.out.println("Sent message: " + message2.getText());
-         
-         //Step 14. Close the subscriber - the server could even be stopped at this point!
+
+         // Step 14. Close the subscriber - the server could even be stopped at this point!
          subscriber.close();
-         
-         //Step 15. Create a new subscriber on the *same* durable subscription.
-         
+
+         // Step 15. Create a new subscriber on the *same* durable subscription.
+
          subscriber = session.createDurableSubscriber(topic, "subscriber-1");
-         
-         //Step 16. Consume the message
-         
+
+         // Step 16. Consume the message
+
          messageReceived = (TextMessage)subscriber.receive();
-         
+
          System.out.println("Received message: " + messageReceived.getText());
-         
-         //Step 17. Close the subscriber
+
+         // Step 17. Close the subscriber
          subscriber.close();
-         
-         //Step 18. Delete the durable subscription
+
+         // Step 18. Delete the durable subscription
          session.unsubscribe("subscriber-1");
       }
       finally
