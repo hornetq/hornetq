ee54f987f07977a59cb90ce353fc65aeec8623a8
Howard Gao <hgao@redhat.com>
2009-04-24 16:56:45 +0000

    added xa-with-jta example

---
+++ examples/jms/xa-with-jta/src/org/jboss/jms/example/XAwithJTAExample.java
@@ -0,0 +1,167 @@
+/*
+   * JBoss, Home of Professional Open Source
+   * Copyright 2005-2008, Red Hat Middleware LLC, and individual contributors
+   * by the @authors tag. See the copyright.txt in the distribution for a
+   * full listing of individual contributors.
+   *
+   * This is free software; you can redistribute it and/or modify it
+   * under the terms of the GNU Lesser General Public License as
+   * published by the Free Software Foundation; either version 2.1 of
+   * the License, or (at your option) any later version.
+   *
+   * This software is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   * Lesser General Public License for more details.
+   *
+   * You should have received a copy of the GNU Lesser General Public
+   * License along with this software; if not, write to the Free
+   * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+   * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+   */
+package org.jboss.jms.example;
+
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.XAConnection;
+import javax.jms.XAConnectionFactory;
+import javax.jms.XASession;
+import javax.naming.InitialContext;
+import javax.transaction.Transaction;
+import javax.transaction.xa.XAResource;
+
+import com.arjuna.ats.jta.TransactionManager;
+
+/**
+ * A simple JMS example showing the JBoss Messaging XA support with JTA.
+ *
+ * @author <a href="hgao@redhat.com">Howard Gao</a>
+ */
+public class XAwithJTAExample extends JMSExample
+{
+   private volatile boolean result = true;
+   
+   public static void main(String[] args)
+   {
+      new XAwithJTAExample().run(args);
+   }
+
+   public boolean runExample() throws Exception
+   {
+      XAConnection connection = null;
+      InitialContext initialContext = null;
+      try
+      {
+         //Step 1. Create an initial context to perform the JNDI lookup.
+         initialContext = getContext(0);
+
+         //Step 2. Lookup on the queue
+         Queue queue = (Queue) initialContext.lookup("/queue/exampleQueue");
+
+         //Step 3. Perform a lookup on the XA Connection Factory
+         XAConnectionFactory cf = (XAConnectionFactory) initialContext.lookup("/XAConnectionFactory");
+
+         //Step 4.Create a JMS XAConnection
+         connection = cf.createXAConnection();
+         
+         //Step 5. Start the connection
+         connection.start();
+
+         //Step 6. Create a JMS XASession
+         XASession xaSession = connection.createXASession();
+         
+         //Step 7. Create a normal session
+         Session normalSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         
+         //Step 8. Create a normal Message Producer
+         MessageProducer normalProducer = normalSession.createProducer(queue);
+
+         //Step 9. Get the JMS Session
+         Session session = xaSession.getSession();
+         
+         //Step 10. Create a message consumer
+         MessageConsumer xaConsumer = session.createConsumer(queue);
+         
+         //Step 11. Create two Text Messages
+         TextMessage helloMessage = session.createTextMessage("hello");
+         TextMessage worldMessage = session.createTextMessage("world");
+         
+         //Step 12. Get the Transaction Manager
+         javax.transaction.TransactionManager txMgr = TransactionManager.transactionManager();
+
+         //Step 13. Start a transaction 
+         txMgr.begin();
+         
+         //Step 14. Get the JMS XAResource
+         XAResource xaRes = xaSession.getXAResource();
+         
+         //Step 15. enlist the resource in the Transaction work
+         Transaction transaction = txMgr.getTransaction();
+         transaction.enlistResource(xaRes);
+         
+         //Step 16. Send two messages.
+         normalProducer.send(helloMessage);
+         normalProducer.send(worldMessage);
+
+         //Step 17. Receive the message
+         TextMessage rm1 = (TextMessage)xaConsumer.receive();
+         System.out.println("Message received: " + rm1.getText());
+         TextMessage rm2 = (TextMessage)xaConsumer.receive();
+         System.out.println("Message received: " + rm2.getText());
+         
+         //Step 18. Stop the work
+         transaction.delistResource(xaRes, XAResource.TMSUCCESS);
+         
+         //Step 19. Roll back the transaction
+         txMgr.rollback();
+         
+         //Step 20. Create another transaction
+         txMgr.begin();
+         transaction = txMgr.getTransaction();
+         
+         //Step 21. Enlist the resource to start the transaction work
+         transaction.enlistResource(xaRes);
+         
+         //Step 22. receive those messages again
+         rm1 = (TextMessage)xaConsumer.receive();
+         System.out.println("Message received again: " + rm1.getText());
+         rm2 = (TextMessage)xaConsumer.receive();
+         System.out.println("Message received again: " + rm2.getText());
+         
+         //Step 23. Stop the work
+         transaction.delistResource(xaRes, XAResource.TMSUCCESS);
+
+         //Step 24. Commit!
+         txMgr.commit();
+         
+         //Step 25. Check no more messages are received.
+         TextMessage rm3 = (TextMessage)xaConsumer.receive(2000);
+         if (rm3 == null)
+         {
+            System.out.println("No message received after commit.");
+         }
+         else
+         {
+            result = false;
+         }
+         
+         return result;
+      }
+      finally
+      {
+         //Step 26. Be sure to close our JMS resources!
+         if (initialContext != null)
+         {
+            initialContext.close();
+         }
+         if(connection != null)
+         {
+            connection.close();
+         }
+      }
+   }
+
+}
