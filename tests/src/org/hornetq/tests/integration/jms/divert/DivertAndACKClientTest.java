d9d4c6fa99e1f0f5d85180f41c4771e0e99fb32f
Clebert Suconic <clebert.suconic@gmail.com>
2009-10-01 22:18:38 +0000

    Adding failure condition on test

--- tests/src/org/hornetq/tests/integration/jms/divert/DivertAndACKClientTest.java
+++ tests/src/org/hornetq/tests/integration/jms/divert/DivertAndACKClientTest.java
@@ -13,17 +13,38 @@
 
 package org.hornetq.tests.integration.jms.divert;
 
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_ACK_BATCH_SIZE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_AUTO_GROUP;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_BLOCK_ON_ACKNOWLEDGE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_BLOCK_ON_NON_PERSISTENT_SEND;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_BLOCK_ON_PERSISTENT_SEND;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_CACHE_LARGE_MESSAGE_CLIENT;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_CLIENT_FAILURE_CHECK_PERIOD;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_CONNECTION_LOAD_BALANCING_POLICY_CLASS_NAME;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_CONNECTION_TTL;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_CONSUMER_MAX_RATE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_CONSUMER_WINDOW_SIZE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_MAX_CONNECTIONS;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_MAX_RETRY_INTERVAL;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_MIN_LARGE_MESSAGE_SIZE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_PRE_ACKNOWLEDGE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_PRODUCER_MAX_RATE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_PRODUCER_WINDOW_SIZE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_SCHEDULED_THREAD_POOL_MAX_SIZE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_THREAD_POOL_MAX_SIZE;
+import static org.hornetq.core.client.impl.ClientSessionFactoryImpl.DEFAULT_USE_GLOBAL_POOLS;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.jms.*;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 
 import org.hornetq.core.config.Configuration;
+import org.hornetq.core.config.TransportConfiguration;
 import org.hornetq.core.config.cluster.DivertConfiguration;
 import org.hornetq.jms.HornetQQueue;
 import org.hornetq.tests.util.JMSTestBase;
+import org.hornetq.utils.Pair;
 
 /**
  * A DivertAndACKClientTest
@@ -50,6 +71,7 @@ public class DivertAndACKClientTest extends JMSTestBase
       HornetQQueue queueSource = (HornetQQueue)createQueue("Source");
       HornetQQueue queueTarget = (HornetQQueue)createQueue("Dest");
 
+     
       Connection connection = cf.createConnection();
       Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
@@ -94,12 +116,52 @@ public class DivertAndACKClientTest extends JMSTestBase
       divertList.add(divert);
 
       config.setDivertConfigurations(divertList);
-
+ 
       return config;
    }
 
    // Private -------------------------------------------------------
 
    // Inner classes -------------------------------------------------
+   protected void createCF(List<Pair<TransportConfiguration, TransportConfiguration>> connectorConfigs,
+                           List<String> jndiBindings) throws Exception
+     {
+        int retryInterval = 1000;
+        double retryIntervalMultiplier = 1.0;
+        int reconnectAttempts = -1;
+        boolean failoverOnServerShutdown = true;
+        int callTimeout = 30000;
+
+        jmsServer.createConnectionFactory("ManualReconnectionToSingleServerTest",
+                                              connectorConfigs,
+                                              null,
+                                              DEFAULT_CLIENT_FAILURE_CHECK_PERIOD,
+                                              DEFAULT_CONNECTION_TTL,
+                                              callTimeout,
+                                              DEFAULT_MAX_CONNECTIONS,
+                                              DEFAULT_CACHE_LARGE_MESSAGE_CLIENT,
+                                              DEFAULT_MIN_LARGE_MESSAGE_SIZE,
+                                              DEFAULT_CONSUMER_WINDOW_SIZE,
+                                              DEFAULT_CONSUMER_MAX_RATE,
+                                              DEFAULT_PRODUCER_WINDOW_SIZE,
+                                              DEFAULT_PRODUCER_MAX_RATE,
+                                              false, // TODO: set this to true, and the test will fail
+                                              DEFAULT_BLOCK_ON_PERSISTENT_SEND,
+                                              DEFAULT_BLOCK_ON_NON_PERSISTENT_SEND,
+                                              DEFAULT_AUTO_GROUP,
+                                              DEFAULT_PRE_ACKNOWLEDGE,
+                                              DEFAULT_CONNECTION_LOAD_BALANCING_POLICY_CLASS_NAME,
+                                              DEFAULT_ACK_BATCH_SIZE,
+                                              DEFAULT_ACK_BATCH_SIZE,
+                                              DEFAULT_USE_GLOBAL_POOLS,
+                                              DEFAULT_SCHEDULED_THREAD_POOL_MAX_SIZE,
+                                              DEFAULT_THREAD_POOL_MAX_SIZE,
+                                              retryInterval,
+                                              retryIntervalMultiplier,
+                                              DEFAULT_MAX_RETRY_INTERVAL,
+                                              reconnectAttempts,
+                                              failoverOnServerShutdown,
+                                              jndiBindings);
+     }
 
 }
