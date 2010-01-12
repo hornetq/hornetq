138d1d78d64c41572ca697b49b0c1e2cdc42a259
Jeff Mesnil <jmesnil@gmail.com>
2010-01-12 13:23:13 +0000

    updated client-side load-balancing example
    * rewrite the example to show clearly load-balancing behaviour
      (without round-robin the message consumption)
    * update example + doc to make it clear load-balancing is
      at the session level for a single JMS connection

--- examples/jms/client-side-load-balancing/src/org/hornetq/jms/example/ClientSideLoadBalancingExample.java
+++ examples/jms/client-side-load-balancing/src/org/hornetq/jms/example/ClientSideLoadBalancingExample.java
@@ -14,6 +14,7 @@ package org.hornetq.jms.example;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
@@ -24,14 +25,11 @@ import javax.naming.InitialContext;
 import org.hornetq.common.example.HornetQExample;
 
 /**
- * This example demonstrates how subsequent connections created from a single connection factory can be load
+ * This example demonstrates how sessions created from a single connection can be load
  * balanced across the different nodes of the cluster.
  * 
  * In this example there are three nodes and we use a round-robin client side load-balancing
  * policy.
- * 
- * Note that the three nodes are deliberately not connected as a cluster so we don't have any server-side
- * load balancing going on.
  *
  * @author <a href="tim.fox@jboss.com>Tim Fox</a>
  */
@@ -47,11 +45,7 @@ public class ClientSideLoadBalancingExample extends HornetQExample
    {
       InitialContext initialContext = null;
 
-      Connection connectionA = null;
-
-      Connection connectionB = null;
-
-      Connection connectionC = null;
+      Connection connection = null;
 
       try
       {
@@ -65,107 +59,60 @@ public class ClientSideLoadBalancingExample extends HornetQExample
          ConnectionFactory connectionFactory = (ConnectionFactory)initialContext.lookup("/ConnectionFactory");
 
          // Wait a little while to make sure broadcasts from all nodes have reached the client
-         Thread.sleep(2000);
+         Thread.sleep(5000);
 
-         // Step 4. We create three connections, since we are using round-robin load-balancing this should
-         // result in each connection being connected to a different node of the cluster
+         // Step 4. We create a single connection
+         connection = connectionFactory.createConnection();
+         
+         // Step 5. We create 3 JMS Sessions from the same connection. Since we are using round-robin 
+         // load-balancing this should result in each sessions being connected to a different node of the cluster
+         Session sessionA = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         Session sessionB = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+         Session sessionC = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-         connectionA = connectionFactory.createConnection();
-
-         connectionB = connectionFactory.createConnection();
-
-         connectionC = connectionFactory.createConnection();
-
-         // Step 5. We create a JMS Session on each of those connections
-         Session sessionA = connectionA.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-         Session sessionB = connectionB.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-         Session sessionC = connectionC.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-         // Step 6. We start the connections to ensure delivery occurs on them
-         connectionA.start();
-
-         connectionB.start();
-
-         connectionC.start();
-
-         // Step 7. We create JMS MessageConsumer objects on the sessions
-         MessageConsumer consumerA = sessionA.createConsumer(queue);
-
-         MessageConsumer consumerB = sessionB.createConsumer(queue);
-
-         MessageConsumer consumerC = sessionC.createConsumer(queue);
-
-         // Step 8. We create JMS MessageProducer objects on the sessions
+         // Step 6. We create JMS MessageProducer objects on the sessions
          MessageProducer producerA = sessionA.createProducer(queue);
-
          MessageProducer producerB = sessionB.createProducer(queue);
-
          MessageProducer producerC = sessionC.createProducer(queue);
 
-         // Step 9. We send some messages on each producer
-
+         // Step 7. We send some messages on each producer
          final int numMessages = 10;
 
          for (int i = 0; i < numMessages; i++)
          {
             TextMessage messageA = sessionA.createTextMessage("A:This is text message " + i);
-
             producerA.send(messageA);
-
             System.out.println("Sent message: " + messageA.getText());
 
             TextMessage messageB = sessionB.createTextMessage("B:This is text message " + i);
-
             producerB.send(messageB);
-
             System.out.println("Sent message: " + messageB.getText());
 
             TextMessage messageC = sessionC.createTextMessage("C:This is text message " + i);
-
             producerC.send(messageC);
-
             System.out.println("Sent message: " + messageC.getText());
          }
-
-         // Step 10. We now consume the messages from each node. The connections must be on different nodes
-         // since if they shared nodes then the consumers would receive the messages sent from different connections.
-
-         for (int i = 0; i < numMessages; i++)
-         {
-            TextMessage messageA = (TextMessage)consumerA.receive(5000);
-
-            System.out.println("Got message: " + messageA.getText() + " from node A");
-
-            TextMessage messageB = (TextMessage)consumerB.receive(5000);
-
-            System.out.println("Got message: " + messageB.getText() + " from node B");
-
-            TextMessage messageC = (TextMessage)consumerC.receive(5000);
-
-            System.out.println("Got message: " + messageC.getText() + " from node C");
-         }
+         
+         // Step 8. We start the connection to consume messages
+         connection.start();
+         
+         // Step 9. We consume messages from the 3 session, one at a time.
+         // We try to consume one more message than expected from each session. If
+         // the session were not properly load-balanced, we would be missing a 
+         // message from one of the sessions at the end.
+         consume(sessionA, queue, numMessages, "A");
+         consume(sessionB, queue, numMessages, "B");
+         consume(sessionC, queue, numMessages, "C");
 
          return true;
       }
       finally
       {
-         // Step 11. Be sure to close our resources!
+         // Step 10. Be sure to close our resources!
 
-         if (connectionA != null)
+         if (connection != null)
          {
-            connectionA.close();
-         }
-
-         if (connectionB != null)
-         {
-            connectionB.close();
-         }
-
-         if (connectionC != null)
-         {
-            connectionC.close();
+            connection.close();
          }
 
          if (initialContext != null)
@@ -174,5 +121,18 @@ public class ClientSideLoadBalancingExample extends HornetQExample
          }
       }
    }
+   
+   private void consume(Session session, Queue queue, int numMessages, String node) throws JMSException
+   {
+      MessageConsumer consumer = session.createConsumer(queue);
 
+      for (int i = 0; i < numMessages; i++)
+      {
+         TextMessage message = (TextMessage)consumer.receive(2000);
+         System.out.println("Got message: " + message.getText() + " from node " + node);
+      }
+      
+      System.out.println("receive other message from node " + node + ": " + consumer.receive(2000));
+
+   }
 }
