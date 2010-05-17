ad81660ca10e1ba65785b505f7aa31e4ac594113
Clebert Suconic <clebert.suconic@gmail.com>
2010-05-17 15:21:45 +0000

    tweak

--- src/main/org/hornetq/core/journal/impl/JournalImpl.java
+++ src/main/org/hornetq/core/journal/impl/JournalImpl.java
@@ -35,7 +35,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
