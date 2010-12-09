77f5082cb815cc432d1481829489301e005610a9
Andy Taylor <andy.taylor@jboss.org>
2010-12-09 16:47:56 +0000

    updated static discovery example

--- examples/jms/clustered-static-discovery/src/org/hornetq/jms/example/StaticClusteredQueueExample.java
+++ examples/jms/clustered-static-discovery/src/org/hornetq/jms/example/StaticClusteredQueueExample.java
@@ -39,18 +39,22 @@ public class StaticClusteredQueueExample extends HornetQExample
    @Override
    public boolean runExample() throws Exception
    {
+      Connection initialConnection = null;
+
       Connection connection0 = null;
 
       Connection connection1 = null;
 
-      InitialContext ic0 = null;
+      Connection connection2 = null;
 
-      InitialContext ic1 = null;
+      Connection connection3 = null;
+
+      InitialContext ic0 = null;
 
       try
       {
          // Step 1. Get an initial context for looking up JNDI from server 0
-         ic0 = getContext(0);
+         ic0 = getContext(3);
 
          // Step 2. Look-up the JMS Queue object from JNDI
          Queue queue = (Queue)ic0.lookup("/queue/exampleQueue");
@@ -58,17 +62,22 @@ public class StaticClusteredQueueExample extends HornetQExample
          // Step 3. Look-up a JMS Connection Factory object from JNDI on server 0
          ConnectionFactory cf0 = (ConnectionFactory)ic0.lookup("/ConnectionFactory");
 
-         // Step 4. Get an initial context for looking up JNDI from server 1
-         ic1 = getContext(1);
+         //grab an initial connection and wait, in reality you wouldn't do it this way but since we want to ensure an
+         // equal load balance we do this and then create 4 connections round robined
+         initialConnection = cf0.createConnection();
 
-         // Step 5. Look-up a JMS Connection Factory object from JNDI on server 1
-         ConnectionFactory cf1 = (ConnectionFactory)ic1.lookup("/ConnectionFactory");
-
+         Thread.sleep(2000);
          // Step 6. We create a JMS Connection connection0 which is a connection to server 0
          connection0 = cf0.createConnection();
 
          // Step 7. We create a JMS Connection connection1 which is a connection to server 1
-         connection1 = cf1.createConnection();
+         connection1 = cf0.createConnection();
+
+         // Step 6. We create a JMS Connection connection0 which is a connection to server 0
+         connection2 = cf0.createConnection();
+
+         // Step 7. We create a JMS Connection connection1 which is a connection to server 1
+         connection3 = cf0.createConnection();
 
          // Step 8. We create a JMS Session on server 0
          Session session0 = connection0.createSession(false, Session.AUTO_ACKNOWLEDGE);
@@ -76,24 +85,39 @@ public class StaticClusteredQueueExample extends HornetQExample
          // Step 9. We create a JMS Session on server 1
          Session session1 = connection1.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
+
+         // Step 8. We create a JMS Session on server 0
+         Session session2 = connection2.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         // Step 9. We create a JMS Session on server 1
+         Session session3 = connection3.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
          // Step 10. We start the connections to ensure delivery occurs on them
          connection0.start();
 
          connection1.start();
+
+         connection2.start();
+
+         connection3.start();
 
          // Step 11. We create JMS MessageConsumer objects on server 0 and server 1
          MessageConsumer consumer0 = session0.createConsumer(queue);
 
          MessageConsumer consumer1 = session1.createConsumer(queue);
 
-         Thread.sleep(1000);
+         MessageConsumer consumer2 = session2.createConsumer(queue);
 
-         // Step 12. We create a JMS MessageProducer object on server 0
-         MessageProducer producer = session0.createProducer(queue);
+         MessageConsumer consumer3 = session3.createConsumer(queue);
+
+         Thread.sleep(2000);
+
+         // Step 12. We create a JMS MessageProducer object on server 3
+         MessageProducer producer = session3.createProducer(queue);
 
          // Step 13. We send some messages to server 0
 
-         final int numMessages = 10;
+         final int numMessages = 20;
 
          for (int i = 0; i < numMessages; i++)
          {
@@ -103,21 +127,37 @@ public class StaticClusteredQueueExample extends HornetQExample
 
             System.out.println("Sent message: " + message.getText());
          }
-
+         Thread.sleep(2000);
          // Step 14. We now consume those messages on *both* server 0 and server 1.
          // We note the messages have been distributed between servers in a round robin fashion
          // JMS Queues implement point-to-point message where each message is only ever consumed by a
          // maximum of one consumer
+         int con0Node = getServer(connection0);
+         int con1Node = getServer(connection1);
+         int con2Node = getServer(connection2);
+         int con3Node = getServer(connection3);
 
-         for (int i = 0; i < numMessages; i += 2)
+         if(con0Node + con1Node + con2Node + con3Node != 6)
+         {
+            return false;
+         }
+         for (int i = 0; i < numMessages; i += 4)
          {
             TextMessage message0 = (TextMessage)consumer0.receive(5000);
 
-            System.out.println("Got message: " + message0.getText() + " from node 0");
+            System.out.println("Got message: " + message0.getText() + " from node " + con0Node);
 
             TextMessage message1 = (TextMessage)consumer1.receive(5000);
 
-            System.out.println("Got message: " + message1.getText() + " from node 1");
+            System.out.println("Got message: " + message1.getText() + " from node " + con1Node);
+
+            TextMessage message2 = (TextMessage)consumer2.receive(5000);
+
+            System.out.println("Got message: " + message2.getText() + " from node " + con2Node);
+
+            TextMessage message3 = (TextMessage)consumer3.receive(5000);
+
+            System.out.println("Got message: " + message3.getText() + " from node " + con3Node);
          }
 
          return true;
@@ -125,6 +165,11 @@ public class StaticClusteredQueueExample extends HornetQExample
       finally
       {
          // Step 15. Be sure to close our resources!
+
+         if (initialConnection != null)
+         {
+            initialConnection.close();
+         }
 
          if (connection0 != null)
          {
@@ -136,14 +181,19 @@ public class StaticClusteredQueueExample extends HornetQExample
             connection1.close();
          }
 
+         if (connection2 != null)
+         {
+            connection2.close();
+         }
+
+         if (connection3 != null)
+         {
+            connection3.close();
+         }
+
          if (ic0 != null)
          {
             ic0.close();
-         }
-
-         if (ic1 != null)
-         {
-            ic1.close();
          }
       }
    }
