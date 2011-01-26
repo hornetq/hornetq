8a5f9074888c2a65ee44ec84ab71a8cd88bf263d
Clebert Suconic <clebert.suconic@gmail.com>
2011-01-26 03:28:49 +0000

    removing System.out

--- src/main/org/hornetq/core/paging/cursor/impl/PageCursorProviderImpl.java
+++ src/main/org/hornetq/core/paging/cursor/impl/PageCursorProviderImpl.java
@@ -272,7 +272,7 @@ public class PageCursorProviderImpl implements PageCursorProvider
                if (complete)
                {
 
-                  System.out.println("Disabling depage!");
+                  log.debug("Address " + pagingStore.getAddress() + " is leaving page mode as all messages are consumed and acknowledged from the page store");
                   pagingStore.forceAnotherPage();
 
                   Page currentPage = pagingStore.getCurrentPage();
