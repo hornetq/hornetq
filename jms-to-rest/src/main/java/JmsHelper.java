bc1ff542eabd3f065e1db99fa600eeef12bf9cc6
Bill Burke <bill.burke@jboss.com>
2011-03-31 14:30:13 +0000

    fix examples

--- jms-to-rest/src/main/java/JmsHelper.java
+++ jms-to-rest/src/main/java/JmsHelper.java
@@ -1,9 +1,11 @@
 import org.hornetq.api.core.TransportConfiguration;
 import org.hornetq.api.core.client.ClientSessionFactory;
 import org.hornetq.api.core.client.HornetQClient;
+import org.hornetq.core.client.impl.ClientSessionFactoryImpl;
 import org.hornetq.core.config.impl.FileConfiguration;
 import org.hornetq.jms.client.HornetQConnectionFactory;
 import org.hornetq.jms.client.HornetQDestination;
+import org.hornetq.jms.client.HornetQJMSConnectionFactory;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -20,8 +22,7 @@ public class JmsHelper
       config.setConfigurationUrl(configFile);
       config.start();
       TransportConfiguration transport = config.getConnectorConfigurations().get("netty-connector");
-      ClientSessionFactory factory = HornetQClient.createClientSessionFactory(transport);
-      return new HornetQConnectionFactory(factory);
+      return new HornetQJMSConnectionFactory(HornetQClient.createServerLocatorWithoutHA(transport));
 
    }
 
