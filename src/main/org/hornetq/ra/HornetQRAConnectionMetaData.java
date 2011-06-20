6d8fbba4635bb8398bf1da86d858eef039c13df2
Andy Taylor <andy.taylor@jboss.org>
2011-06-20 18:01:14 +0000

    updated versioning 2. 2.2.6.Final

--- src/main/org/hornetq/ra/HornetQRAConnectionMetaData.java
+++ src/main/org/hornetq/ra/HornetQRAConnectionMetaData.java
@@ -113,7 +113,7 @@ public class HornetQRAConnectionMetaData implements ConnectionMetaData
          HornetQRAConnectionMetaData.log.trace("getJMSProviderName()");
       }
 
-      return "2.2.5.Final";
+      return "2.2.6.Final";
    }
 
    /**
