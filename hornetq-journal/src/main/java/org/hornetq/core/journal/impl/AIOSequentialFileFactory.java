001aa38b4e48caa6959860a8c372f72ef36cf6b3
Francisco Borges <francisco.borges@redhat.com>
2012-02-01 16:49:17 +0000

    add .toString()

--- hornetq-journal/src/main/java/org/hornetq/core/journal/impl/AIOSequentialFileFactory.java
+++ hornetq-journal/src/main/java/org/hornetq/core/journal/impl/AIOSequentialFileFactory.java
@@ -349,4 +349,10 @@ public class AIOSequentialFileFactory extends AbstractSequentialFileFactory
 
    }
 
+   @Override
+   public String toString()
+   {
+      return AIOSequentialFileFactory.class.getSimpleName() + "(buffersControl.stopped=" + buffersControl.stopped +
+               "):" + super.toString();
+   }
 }
