3a753acd6b7e77490caa4933fdc1ebaf0d7f5615
Clebert Suconic <clebert.suconic@gmail.com>
2012-02-27 20:40:42 +0000

    https://issues.jboss.org/browse/JBPAPP-8243 - duh

--- hornetq-core/src/main/java/org/hornetq/core/client/impl/ClientSessionImpl.java
+++ hornetq-core/src/main/java/org/hornetq/core/client/impl/ClientSessionImpl.java
@@ -1908,7 +1908,7 @@ public class ClientSessionImpl implements ClientSessionInternal, FailureListener
 
    private void doCleanup(boolean failingOver)
    {
-      if (remotingConnection == null)
+      if (remotingConnection != null)
       {
          remotingConnection.removeFailureListener(this);
       }
