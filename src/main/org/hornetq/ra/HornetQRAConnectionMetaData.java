dceb71c7f343a3fc3f4830c3533a59de36eaf294
Andy Taylor <ataylor@redhat.com>
2012-04-02 12:11:04 +0100

    updated version 2 2.2.14.Final

--- src/main/org/hornetq/ra/HornetQRAConnectionMetaData.java
+++ src/main/org/hornetq/ra/HornetQRAConnectionMetaData.java
@@ -113,7 +113,7 @@ public class HornetQRAConnectionMetaData implements ConnectionMetaData
          HornetQRAConnectionMetaData.log.trace("getJMSProviderName()");
       }
 
-      return "2.2.6.Final";
+      return "2.2.14.Final";
    }
 
    /**
