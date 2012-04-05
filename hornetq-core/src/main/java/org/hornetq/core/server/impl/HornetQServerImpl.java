655221f081608a496b3d9fada87719556729967a
Francisco Borges <francisco.borges@redhat.com>
2012-04-05 16:29:11 +0200

    Add time-out for joining backup activation thread. Log warning if it does time-out.

--- hornetq-core/src/main/java/org/hornetq/core/server/impl/HornetQServerImpl.java
+++ hornetq-core/src/main/java/org/hornetq/core/server/impl/HornetQServerImpl.java
@@ -666,7 +666,13 @@ public class HornetQServerImpl implements HornetQServer
          }
          if (backupActivationThread != null)
          {
-            backupActivationThread.join();
+
+            backupActivationThread.join(30000);
+            if (backupActivationThread.isAlive())
+            {
+               log.warn(this + " backup activation thread did not finish.");
+               backupActivationThread.interrupt();
+            }
          }
 
          stopComponent(nodeManager);
