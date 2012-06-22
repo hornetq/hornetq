6240094ba9ba02973611e6a12962b2c9d627f977
Francisco Borges <francisco.borges@redhat.com>
2012-06-22 11:59:18 +0200

    Fix type warnings

--- tests/unit-tests/src/test/java/org/hornetq/tests/unit/util/NonSerializableFactory.java
+++ tests/unit-tests/src/test/java/org/hornetq/tests/unit/util/NonSerializableFactory.java
@@ -89,7 +89,10 @@ public class NonSerializableFactory implements ObjectFactory
       return NonSerializableFactory.getWrapperMap().get(name);
    }
 
-   public Object getObjectInstance(final Object obj, final Name name, final Context nameCtx, final Hashtable env) throws Exception
+   public
+            Object
+            getObjectInstance(final Object obj, final Name name, final Context nameCtx, final Hashtable<?, ?> env)
+                                                                                                                  throws Exception
    {
       Reference ref = (Reference)obj;
       RefAddr addr = ref.get("nns");
@@ -97,10 +100,10 @@ public class NonSerializableFactory implements ObjectFactory
       return NonSerializableFactory.getWrapperMap().get(key);
    }
 
-   public static Map getWrapperMap()
+   public static Map<String, Object> getWrapperMap()
    {
       return NonSerializableFactory.wrapperMap;
    }
 
-   private static Map wrapperMap = Collections.synchronizedMap(new HashMap());
+   private static Map<String, Object> wrapperMap = Collections.synchronizedMap(new HashMap<String, Object>());
 }
