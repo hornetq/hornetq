9dd7654967ea451fdb526fad13a7b72f0f7e00f5
Justin Bertram <jbertram@redhat.com>
2012-07-09 22:24:55 -0500

    adding a few resources for testing

---
+++ hornetq-rest/hornetq-rest/src/test/java/org/hornetq/rest/test/Util.java
@@ -0,0 +1,32 @@
+package org.hornetq.rest.test;
+
+import org.hornetq.rest.util.Constants;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.spi.Link;
+import org.junit.Assert;
+
+public class Util
+{
+   static ClientResponse head(ClientRequest request) throws Exception
+   {
+      ClientResponse response = request.head();
+      response.releaseConnection();
+      Assert.assertEquals(200, response.getStatus());
+      return response;
+   }
+
+   static String getUrlPath(String queueName)
+   {
+      return Constants.PATH_FOR_QUEUES + "/" + queueName;
+   }
+
+   static ClientResponse setAutoAck(Link link, boolean ack) throws Exception
+   {
+      ClientResponse response;
+      response = link.request().formParameter("autoAck", Boolean.toString(ack)).post();
+      response.releaseConnection();
+      Assert.assertEquals(201, response.getStatus());
+      return response;
+   }
+}
