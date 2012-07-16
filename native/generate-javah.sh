114895f46ccde28dc346fb2b42ce967ff21f1004
Francisco Borges <francisco.borges@redhat.com>
2012-07-16 17:34:25 +0200

    add verbose to temp script

--- native/generate-javah.sh
+++ native/generate-javah.sh
@@ -1,2 +1,2 @@
 # this is a temporary procedure for now, until the automate the javah on maven
-javah -classpath ~/.m2/repository/org/jboss/logging/jboss-logging/3.1.0.GA/jboss-logging-3.1.0.GA.jar:../hornetq-journal/target/classes org.hornetq.core.asyncio.impl.AsynchronousFileImpl
+javah -v -classpath ~/.m2/repository/org/jboss/logging/jboss-logging/3.1.0.GA/jboss-logging-3.1.0.GA.jar:../hornetq-journal/target/classes org.hornetq.core.asyncio.impl.AsynchronousFileImpl
