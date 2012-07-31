3c260b02b926e8b85cae1c29a1f99881f5211fea
clebert suconic <clebert.suconic@gmail.com>
2012-07-31 15:16:39 -0500

    JBPAPP-9603 - fix on deleting temporary queues

--- src/main/org/hornetq/core/server/impl/HornetQServerImpl.java
+++ src/main/org/hornetq/core/server/impl/HornetQServerImpl.java
@@ -964,6 +964,15 @@ public class HornetQServerImpl implements HornetQServer
       return createQueue(address, queueName, filterString, durable, temporary, true);
    }
 
+
+   public void destroyQueue(final SimpleString queueName) throws Exception
+   {
+      // The session is passed as an argument to verify if the user has authorization to delete the queue
+      // in some cases (such as temporary queues) this should happen regardless of the authorization
+      // since that will only happen during a session close, which will be used to cleanup on temporary queues
+      destroyQueue(queueName, null);
+   }
+
    public void destroyQueue(final SimpleString queueName, final ServerSession session) throws Exception
    {
       addressSettingsRepository.clearCache();
@@ -977,17 +986,20 @@ public class HornetQServerImpl implements HornetQServer
 
       Queue queue = (Queue)binding.getBindable();
 
-      if (queue.getConsumerCount() != 0)
-      {
-         throw new HornetQException(HornetQException.ILLEGAL_STATE, "Cannot delete queue " + queue.getName() +
-                                                                    " on binding " +
-                                                                    queueName +
-                                                                    " - it has consumers = " +
-                                                                    binding.getClass().getName());
-      }
-
       if (session != null)
       {
+         // This check is only valid if session != null
+         // When sessio = null, this check is being done outside of session usage, through temp queues for instance
+         // and this check is out of context.
+         if (queue.getConsumerCount() != 0)
+         {
+            throw new HornetQException(HornetQException.ILLEGAL_STATE, "Cannot delete queue " + queue.getName() +
+                                                                       " on binding " +
+                                                                       queueName +
+                                                                       " - it has consumers = " +
+                                                                       binding.getClass().getName());
+         }
+
          if (queue.isDurable())
          {
             // make sure the user has privileges to delete this queue
