f5ced3795eb28c4f0a958d4ff9adf0d3dbde09ad
Francisco Borges <francisco.borges@redhat.com>
2012-10-12 17:34:01 +0200

    Revert "FIX: HornetQXATopicConnection was returning a non-XA TopicSession"
    This reverts commit b2f0faaa96466e58a97fbe7cf7c6b869bf5d0b1a.

--- src/main/org/hornetq/jms/client/HornetQXATopicConnection.java
+++ src/main/org/hornetq/jms/client/HornetQXATopicConnection.java
@@ -13,8 +13,6 @@
 
 package org.hornetq.jms.client;
 
-import javax.jms.JMSException;
-import javax.jms.TopicSession;
 import javax.jms.XATopicConnection;
 
 import org.hornetq.api.core.client.ClientSessionFactory;
@@ -33,13 +31,7 @@ public class HornetQXATopicConnection extends HornetQConnection implements XATop
                                    final int dupsOKBatchSize,
                                    final int transactionBatchSize,
                                    final ClientSessionFactory sessionFactory)
-   {
-      super(username, password, connectionType, clientID, dupsOKBatchSize, transactionBatchSize, sessionFactory);
-   }
-
-   public TopicSession createTopicSession(final boolean transacted, final int acknowledgeMode) throws JMSException
-   {
-      checkClosed();
-      return createSessionInternal(transacted, acknowledgeMode, true, HornetQSession.TYPE_TOPIC_SESSION);
-   }
+          {
+             super(username, password, connectionType, clientID, dupsOKBatchSize, transactionBatchSize, sessionFactory);
+          }
 }
