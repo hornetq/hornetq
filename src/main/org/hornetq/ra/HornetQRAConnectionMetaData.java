9ce64fee196f19b82c2e0c2884cc2c87b0cebd3a
clebert suconic <clebert.suconic@gmail.com>
2012-11-09 08:22:19 -0600

    Changing version back to snapshot

--- src/main/org/hornetq/ra/HornetQRAConnectionMetaData.java
+++ src/main/org/hornetq/ra/HornetQRAConnectionMetaData.java
@@ -113,7 +113,7 @@ public class HornetQRAConnectionMetaData implements ConnectionMetaData
          HornetQRAConnectionMetaData.log.trace("getJMSProviderName()");
       }
 
-      return "2.2.23.Final";
+      return "2.2.24.Snapshot";
    }
 
    /**
