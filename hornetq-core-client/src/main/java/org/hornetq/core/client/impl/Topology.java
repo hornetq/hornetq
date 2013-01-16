hornetq/hornetq

commit 133ce0c9a81d9d52c39325a1924ad6dc75948cd8

cluster connection should pulse its live update to make sure no one missed it

hornetq-server/src/main/java/org/hornetq/core/server/cluster/impl/ClusterConnectionImpl.java
@@ -25,12 +25,15 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import org.hornetq.api.core.DiscoveryGroupConfiguration;
+import org.hornetq.api.core.HornetQInterruptedException;
 import org.hornetq.api.core.Pair;
 import org.hornetq.api.core.SimpleString;
 import org.hornetq.api.core.TransportConfiguration;
@@ -80,6 +83,8 @@ import org.hornetq.utils.TypedProperties;
 public final class ClusterConnectionImpl implements ClusterConnection, AfterConnectInternalListener
 {
    private static final boolean isTrace = HornetQServerLogger.LOGGER.isTraceEnabled();
+
+   private final ExecutorService threadPool;
 
    private final ExecutorFactory executorFactory;
 
@@ -160,10 +165,15 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
    private volatile ServerLocatorInternal backupServerLocator;
    private volatile boolean announcingBackup;
    private volatile boolean stopping = false;
+   private LiveNotifier liveNotifier;
+   private final Object liveNotifierGuard = new Object();
+   private final long clusterNotificationInterval;
+   private final int clusterNotificationAttempts;
 
    /**
     * @param staticTranspConfigs notice if {@code null} this is a cluster which won't connect to
     *           anyone, but that can still accept incoming connections.
+    * @param clusterNotificationInterval
     */
    public ClusterConnectionImpl(final ClusterManager manager, final TransportConfiguration[] staticTranspConfigs,
                                 final TransportConfiguration connector,
@@ -182,6 +192,7 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
                                 final boolean routeWhenNoConsumers,
                                 final int confirmationWindowSize,
                                 final ExecutorFactory executorFactory,
+                                final ExecutorService threadPool,
                                 final HornetQServer server,
                                 final PostOffice postOffice,
                                 final ManagementService managementService,
@@ -191,7 +202,9 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
                                 final boolean backup,
                                 final String clusterUser,
                                 final String clusterPassword,
-                                final boolean allowDirectConnectionsOnly) throws Exception
+                                final boolean allowDirectConnectionsOnly,
+                                final long clusterNotificationInterval,
+                                final int clusterNotificationAttempts) throws Exception
    {
       this.nodeManager = nodeManager;
 
@@ -221,7 +234,13 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
 
       this.executorFactory = executorFactory;
 
+      this.clusterNotificationInterval = clusterNotificationInterval;
+
+      this.clusterNotificationAttempts = clusterNotificationAttempts;
+
       this.executor = executorFactory.getExecutor();
+
+      this.threadPool = threadPool;
 
       this.topology.setExecutor(executor);
 
@@ -282,6 +301,7 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
    /**
     * @param dg discovery group, notice if {@code null} this is a cluster which won't connect to
     *           anyone, but that can still accept incoming connections.
+    * @param clusterNotificationInterval
     */
    public ClusterConnectionImpl(final ClusterManager manager,
                                 DiscoveryGroupConfiguration dg,
@@ -301,6 +321,7 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
                                 final boolean routeWhenNoConsumers,
                                 final int confirmationWindowSize,
                                 final ExecutorFactory executorFactory,
+                                final ExecutorService threadPool,
                                 final HornetQServer server,
                                 final PostOffice postOffice,
                                 final ManagementService managementService,
@@ -310,7 +331,9 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
                                 final boolean backup,
                                 final String clusterUser,
                                 final String clusterPassword,
-                                final boolean allowDirectConnectionsOnly) throws Exception
+                                final boolean allowDirectConnectionsOnly,
+                                final long clusterNotificationInterval,
+                                final int clusterNotificationAttempts) throws Exception
    {
       this.nodeManager = nodeManager;
 
@@ -346,7 +369,13 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
 
       this.executorFactory = executorFactory;
 
+      this.clusterNotificationInterval = clusterNotificationInterval;
+
+      this.clusterNotificationAttempts = clusterNotificationAttempts;
+
       this.executor = executorFactory.getExecutor();
+
+      this.threadPool = threadPool;
 
       this.topology.setExecutor(executor);
 
@@ -414,6 +443,14 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
       if (HornetQServerLogger.LOGGER.isDebugEnabled())
       {
          HornetQServerLogger.LOGGER.debug(this + "::stopping ClusterConnection");
+      }
+
+      synchronized (liveNotifierGuard)
+      {
+         if(liveNotifier != null)
+         {
+            liveNotifier.cancel();
+         }
       }
 
       if (serverLocator != null)
@@ -684,8 +721,17 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
 
       backup = false;
 
-      topology.updateAsLive(manager.getNodeId(), new TopologyMemberImpl(manager.getNodeId(),
-                                                                        manager.getNodeGroupName(), connector, null));
+      synchronized (liveNotifierGuard)
+      {
+         if(stopping)
+         {
+            return;
+         }
+         liveNotifier = new LiveNotifier();
+         threadPool.execute(liveNotifier);
+      }
+
+      liveNotifier.awaitFirstUpdate();
 
       if (backupServerLocator != null)
       {
@@ -1666,5 +1712,50 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
    public boolean verify(String clusterUser0, String clusterPassword0)
    {
       return clusterUser.equals(clusterUser0) && clusterPassword.equals(clusterPassword0);
+   }
+
+   private final class LiveNotifier implements Runnable
+   {
+      private CountDownLatch latch = new CountDownLatch(1);
+
+      @Override
+      public synchronized void run()
+      {
+         int notificationsSent = 0;
+         while(notificationsSent < clusterNotificationAttempts && started && !stopping)
+         {
+            topology.updateAsLive(manager.getNodeId(), new TopologyMemberImpl(manager.getNodeId(),
+                  manager.getNodeGroupName(), connector, null));
+            if(notificationsSent++ == 0)
+            {
+               latch.countDown();
+            }
+            try
+            {
+               wait(clusterNotificationInterval);
+            }
+            catch (InterruptedException e1)
+            {
+               throw new HornetQInterruptedException(e1);
+            }
+         }
+      }
+
+      public synchronized void cancel()
+      {
+         notify();
+      }
+
+      public void awaitFirstUpdate()
+      {
+         try
+         {
+            latch.await(clusterNotificationInterval, TimeUnit.MILLISECONDS);
+         }
+         catch (InterruptedException e)
+         {
+            throw new HornetQInterruptedException(e);
+         }
+      }
    }
 }

hornetq-core-client/src/main/java/org/hornetq/api/config/HornetQDefaultConfiguration.java
@@ -105,6 +105,8 @@ public final class HornetQDefaultConfiguration
    public static final long DEFAULT_FAILBACK_DELAY = 5000; // in milliseconds
    public static final boolean DEFAULT_CHECK_FOR_LIVE_SERVER = false;
    public static final boolean DEFAULT_MASK_PASSWORD = false;
+   public static final long DEFAULT_CLUSTER_NOTIFICATION_INTERVAL = 1000;
+   public static final int DEFAULT_CLUSTER_NOTIFICATION_ATTEMPTS = 2;
 
    //properties passed to acceptor/connectors.
    public static final String PROP_MASK_PASSWORD = "hornetq.usemaskedpassword";

hornetq-server/src/main/java/org/hornetq/core/config/ClusterConnectionConfiguration.java
@@ -68,6 +68,10 @@ public final class ClusterConnectionConfiguration implements Serializable
 
    private final int minLargeMessageSize;
 
+   private final long clusterNotificationInterval;
+
+   private final int clusterNotificationAttempts;
+
    public ClusterConnectionConfiguration(final String name,
                                          final String address,
                                          final String connectorName,
@@ -96,7 +100,9 @@ public final class ClusterConnectionConfiguration implements Serializable
          maxHops,
          confirmationWindowSize,
          staticConnectors,
-         allowDirectConnectionsOnly);
+         allowDirectConnectionsOnly,
+         HornetQDefaultConfiguration.DEFAULT_CLUSTER_NOTIFICATION_INTERVAL,
+         HornetQDefaultConfiguration.DEFAULT_CLUSTER_NOTIFICATION_ATTEMPTS);
    }
 
 
@@ -117,7 +123,9 @@ public final class ClusterConnectionConfiguration implements Serializable
                                          final int maxHops,
                                          final int confirmationWindowSize,
                                          final List<String> staticConnectors,
-                                         final boolean allowDirectConnectionsOnly)
+                                         final boolean allowDirectConnectionsOnly,
+                                         final long clusterNotificationInterval,
+                                         final int clusterNotificationAttempts)
    {
       this.name = name;
       this.address = address;
@@ -147,6 +155,8 @@ public final class ClusterConnectionConfiguration implements Serializable
       this.confirmationWindowSize = confirmationWindowSize;
       this.allowDirectConnectionsOnly = allowDirectConnectionsOnly;
       this.minLargeMessageSize = minLargeMessageSize;
+      this.clusterNotificationInterval = clusterNotificationInterval;
+      this.clusterNotificationAttempts = clusterNotificationAttempts;
    }
 
 
@@ -176,7 +186,9 @@ public final class ClusterConnectionConfiguration implements Serializable
          forwardWhenNoConsumers,
          maxHops,
          confirmationWindowSize,
-         discoveryGroupName);
+         discoveryGroupName,
+         HornetQDefaultConfiguration.DEFAULT_CLUSTER_NOTIFICATION_INTERVAL,
+         HornetQDefaultConfiguration.DEFAULT_CLUSTER_NOTIFICATION_ATTEMPTS);
    }
 
 
@@ -196,7 +208,9 @@ public final class ClusterConnectionConfiguration implements Serializable
                                          final boolean forwardWhenNoConsumers,
                                          final int maxHops,
                                          final int confirmationWindowSize,
-                                         final String discoveryGroupName)
+                                         final String discoveryGroupName,
+                                         final long clusterNotificationInterval,
+                                         final int clusterNotificationAttempts)
    {
       this.name = name;
       this.address = address;
@@ -212,6 +226,8 @@ public final class ClusterConnectionConfiguration implements Serializable
       this.duplicateDetection = duplicateDetection;
       this.forwardWhenNoConsumers = forwardWhenNoConsumers;
       this.discoveryGroupName = discoveryGroupName;
+      this.clusterNotificationInterval = clusterNotificationInterval;
+      this.clusterNotificationAttempts = clusterNotificationAttempts;
       this.staticConnectors = Collections.emptyList();
       this.maxHops = maxHops;
       this.confirmationWindowSize = confirmationWindowSize;
@@ -411,5 +427,18 @@ public final class ClusterConnectionConfiguration implements Serializable
    public void setForwardWhenNoConsumers(boolean forwardWhenNoConsumers)
    {
       this.forwardWhenNoConsumers = forwardWhenNoConsumers;
+   }
+
+   /*
+   * returns the cluster update interval
+   * */
+   public long getClusterNotificationInterval()
+   {
+      return clusterNotificationInterval;
+   }
+
+   public int getClusterNotificationAttempts()
+   {
+      return clusterNotificationAttempts;
    }
 }

hornetq-server/src/main/java/org/hornetq/core/deployers/impl/FileConfigurationParser.java
@@ -1153,6 +1153,10 @@ public final class FileConfigurationParser
                                                                    FileConfiguration.DEFAULT_CONFIRMATION_WINDOW_SIZE,
                                                                    Validators.GT_ZERO);
 
+      long clusterNotificationInterval = XMLConfigurationUtil.getLong(e, "notification-interval", HornetQDefaultConfiguration.DEFAULT_CLUSTER_NOTIFICATION_INTERVAL, Validators.GT_ZERO);
+
+      int clusterNotificationAttempts = XMLConfigurationUtil.getInteger(e, "notification-attempts", HornetQDefaultConfiguration.DEFAULT_CLUSTER_NOTIFICATION_ATTEMPTS, Validators.GT_ZERO);
+
       String discoveryGroupName = null;
 
       List<String> staticConnectorNames = new ArrayList<String>();
@@ -1192,7 +1196,9 @@ public final class FileConfigurationParser
                                                      duplicateDetection, forwardWhenNoConsumers, maxHops,
                                                      confirmationWindowSize,
                                                      staticConnectorNames,
-                                                     allowDirectConnectionsOnly);
+                                                     allowDirectConnectionsOnly,
+                                                     clusterNotificationInterval,
+                                                     clusterNotificationAttempts);
       }
       else
       {
@@ -1210,7 +1216,9 @@ public final class FileConfigurationParser
                                                      forwardWhenNoConsumers,
                                                      maxHops,
                                                      confirmationWindowSize,
-                                                     discoveryGroupName);
+                                                     discoveryGroupName,
+                                                     clusterNotificationInterval,
+                                                     clusterNotificationAttempts);
       }
 
       mainConfig.getClusterConfigurations().add(config);

hornetq-server/src/main/java/org/hornetq/core/server/cluster/ClusterManager.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.hornetq.api.core.BroadcastGroupConfiguration;
@@ -86,6 +87,8 @@ public class ClusterManager implements HornetQComponent
 
    private final Configuration configuration;
 
+   private final ExecutorService threadPool;
+
    enum State
    {
       STOPPED,
@@ -115,12 +118,13 @@ public class ClusterManager implements HornetQComponent
    private final NodeManager nodeManager;
 
    public ClusterManager(final ExecutorFactory executorFactory,
-                             final HornetQServer server,
-                             final PostOffice postOffice,
-                             final ScheduledExecutorService scheduledExecutor,
-                             final ManagementService managementService,
-                             final Configuration configuration,
-                         final NodeManager nodeManager, final boolean backup)
+                         final HornetQServer server,
+                         final PostOffice postOffice,
+                         final ScheduledExecutorService scheduledExecutor,
+                         final ManagementService managementService,
+                         final Configuration configuration,
+                         final NodeManager nodeManager, final boolean backup,
+                         ExecutorService threadPool)
    {
       this.executorFactory = executorFactory;
 
@@ -139,6 +143,8 @@ public class ClusterManager implements HornetQComponent
       this.nodeManager = nodeManager;
 
       this.backup = backup;
+
+      this.threadPool = threadPool;
    }
 
    public String describe()
@@ -707,6 +713,7 @@ public class ClusterManager implements HornetQComponent
                                                        config.isForwardWhenNoConsumers(),
                                                        config.getConfirmationWindowSize(),
                                                        executorFactory,
+                                                       threadPool,
                                                        server,
                                                        postOffice,
                                                        managementService,
@@ -716,7 +723,9 @@ public class ClusterManager implements HornetQComponent
                                                        backup,
                                                        server.getConfiguration().getClusterUser(),
                                                        server.getConfiguration().getClusterPassword(),
-                                                       config.isAllowDirectConnectionsOnly());
+                                                       config.isAllowDirectConnectionsOnly(),
+                                                       config.getClusterNotificationInterval(),
+                                                       config.getClusterNotificationAttempts());
       }
       else
       {
@@ -746,6 +755,7 @@ public class ClusterManager implements HornetQComponent
                                                        config.isForwardWhenNoConsumers(),
                                                        config.getConfirmationWindowSize(),
                                                        executorFactory,
+                                                       threadPool,
                                                        server,
                                                        postOffice,
                                                        managementService,
@@ -755,7 +765,9 @@ public class ClusterManager implements HornetQComponent
                                                        backup,
                                                        server.getConfiguration().getClusterUser(),
                                                        server.getConfiguration().getClusterPassword(),
-                                                       config.isAllowDirectConnectionsOnly());
+                                                       config.isAllowDirectConnectionsOnly(),
+                                                       config.getClusterNotificationInterval(),
+                                                       config.getClusterNotificationAttempts());
       }
 
       if (defaultClusterConnection == null)

hornetq-server/src/main/java/org/hornetq/core/server/impl/HornetQServerImpl.java
@@ -1408,7 +1408,7 @@ public class HornetQServerImpl implements HornetQServer
       // This can't be created until node id is set
       clusterManager =
                new ClusterManager(executorFactory, this, postOffice, scheduledPool, managementService, configuration,
-                                  nodeManager, configuration.isBackup());
+                                  nodeManager, configuration.isBackup(), threadPool);
 
       clusterManager.deploy();
 

hornetq-server/src/main/resources/schema/hornetq-configuration.xsd
@@ -353,6 +353,20 @@
             <xsd:element maxOccurs="1" minOccurs="0" name="max-hops" type="xsd:int"/>
             <xsd:element maxOccurs="1" minOccurs="0" name="confirmation-window-size" type="xsd:int"/>
             <xsd:element maxOccurs="1" minOccurs="0" name="call-failover-timeout" type="xsd:long"/>
+            <xsd:element maxOccurs="1" minOccurs="0" name="notification-interval" type="xsd:long">
+              <xsd:annotation>
+                  <xsd:documentation>
+                      how often the cluster connection will notify the cluster of its existence
+                  </xsd:documentation>
+              </xsd:annotation>
+            </xsd:element>
+            <xsd:element maxOccurs="1" minOccurs="0" name="notification-attempts" type="xsd:int">
+              <xsd:annotation>
+                  <xsd:documentation>
+                      how many times cluster connection will notify the cluster of its existence
+                  </xsd:documentation>
+              </xsd:annotation>
+            </xsd:element>
             <xsd:choice>
                 <xsd:element maxOccurs="1" minOccurs="0" name="static-connectors">
                     <xsd:complexType>

commit 0fdae882f3e6e034cd3bd9fa335cc3bd41700986

Fixing SingleLiveMultipleBackupsFailoverTest... This test was failing after commit 133ce0c9a81d9d52c39325a1924ad6dc75948cd8 (pulsing updates) as that commit was removing / re-adding the node and ignoring certain updates

hornetq-core-client/src/main/java/org/hornetq/core/client/impl/Topology.java
@@ -116,6 +116,23 @@ public final class Topology implements Serializable
       }
    }
 
+   /**
+    * After the node is started, it will resend the notifyLive a couple of times to avoid gossip between two servers
+    * @param nodeId
+    */
+   public void resendNode(final String nodeId)
+   {
+      synchronized (this)
+      {
+         TopologyMemberImpl memberInput = topology.get(nodeId);
+         if (memberInput != null)
+         {
+            memberInput.setUniqueEventID(System.currentTimeMillis());
+            sendMemberUp(nodeId, memberInput);
+         }
+      }
+   }
+
    /** This is called by the server when the node is activated from backup state. It will always succeed */
    public final TopologyMemberImpl updateBackup(final TopologyMemberImpl memberInput)
    {

hornetq-server/src/main/java/org/hornetq/core/server/cluster/impl/ClusterConnectionImpl.java
@@ -165,8 +165,7 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
    private volatile ServerLocatorInternal backupServerLocator;
    private volatile boolean announcingBackup;
    private volatile boolean stopping = false;
-   private LiveNotifier liveNotifier;
-   private final Object liveNotifierGuard = new Object();
+   private LiveNotifier liveNotifier = null;
    private final long clusterNotificationInterval;
    private final int clusterNotificationAttempts;
 
@@ -445,14 +444,6 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
          HornetQServerLogger.LOGGER.debug(this + "::stopping ClusterConnection");
       }
 
-      synchronized (liveNotifierGuard)
-      {
-         if(liveNotifier != null)
-         {
-            liveNotifier.cancel();
-         }
-      }
-
       if (serverLocator != null)
       {
          serverLocator.removeClusterTopologyListener(this);
@@ -721,17 +712,9 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
 
       backup = false;
 
-      synchronized (liveNotifierGuard)
-      {
-         if(stopping)
-         {
-            return;
-         }
-         liveNotifier = new LiveNotifier();
-         threadPool.execute(liveNotifier);
-      }
-
-      liveNotifier.awaitFirstUpdate();
+      liveNotifier = new LiveNotifier();
+      liveNotifier.updateAsLive();
+      liveNotifier.schedule();
 
       if (backupServerLocator != null)
       {
@@ -1716,45 +1699,39 @@ public final class ClusterConnectionImpl implements ClusterConnection, AfterConn
 
    private final class LiveNotifier implements Runnable
    {
-      private CountDownLatch latch = new CountDownLatch(1);
+      int notificationsSent = 0;
+
 
       @Override
-      public synchronized void run()
+      public void run()
       {
-         int notificationsSent = 0;
-         while(notificationsSent < clusterNotificationAttempts && started && !stopping)
+         resendLive();
+
+         schedule();
+      }
+
+      public void schedule()
+      {
+         if (started && !stopping && notificationsSent++ < clusterNotificationAttempts)
+         {
+            scheduledExecutor.schedule(this, clusterNotificationInterval, TimeUnit.MILLISECONDS);
+         }
+      }
+
+      public void updateAsLive()
+      {
+         if (!stopping && started)
          {
             topology.updateAsLive(manager.getNodeId(), new TopologyMemberImpl(manager.getNodeId(),
-                  manager.getNodeGroupName(), connector, null));
-            if(notificationsSent++ == 0)
-            {
-               latch.countDown();
-            }
-            try
-            {
-               wait(clusterNotificationInterval);
-            }
-            catch (InterruptedException e1)
-            {
-               throw new HornetQInterruptedException(e1);
-            }
+               manager.getNodeGroupName(), connector, null));
          }
       }
 
-      public synchronized void cancel()
+      public void resendLive()
       {
-         notify();
-      }
-
-      public void awaitFirstUpdate()
-      {
-         try
+         if (!stopping && started)
          {
-            latch.await(clusterNotificationInterval, TimeUnit.MILLISECONDS);
-         }
-         catch (InterruptedException e)
-         {
-            throw new HornetQInterruptedException(e);
+            topology.resendNode(manager.getNodeId());
          }
       }
    }