677ceb9904e1a4095e47726957dac93c7c118675
Francisco Borges <francisco.borges@redhat.com>
2013-03-06 17:25:47 +0100

    Rename compactorEnabled --> compactorRunning

--- hornetq-server/src/main/java/org/hornetq/core/replication/ReplicationEndpoint.java
+++ hornetq-server/src/main/java/org/hornetq/core/replication/ReplicationEndpoint.java
@@ -861,7 +861,7 @@ public final class ReplicationEndpoint implements ChannelHandler, HornetQCompone
       return journals[journalID];
    }
 
-   public static class JournalSyncFile
+   public static final class JournalSyncFile
    {
 
       private FileChannel channel;
