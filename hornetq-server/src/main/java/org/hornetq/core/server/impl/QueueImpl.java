66ac3bbc57539a35876335d6ad71ca06618618ac
clebert suconic <clebert.suconic@gmail.com>
2013-03-08 09:36:05 -0600

    another test fix avoiding NPE on queue

--- hornetq-server/src/main/java/org/hornetq/core/server/impl/QueueImpl.java
+++ hornetq-server/src/main/java/org/hornetq/core/server/impl/QueueImpl.java
@@ -1126,7 +1126,7 @@ public class QueueImpl implements Queue
          }
 
 
-         if (!pageDestroyed)
+         if (pageIterator != null && !pageDestroyed)
          {
             // System.out.println("QueueMemorySize before depage = " + queueMemorySize.get());
             while (pageIterator.hasNext())
