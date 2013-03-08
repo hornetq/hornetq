5a98ed0b67a95e4e5d7b0258c781865caa9ee40e
clebert suconic <clebert.suconic@gmail.com>
2013-03-08 07:21:39 -0600

    fixing test that are failing after topic=null change

--- src/main/org/hornetq/core/server/impl/QueueImpl.java
+++ src/main/org/hornetq/core/server/impl/QueueImpl.java
@@ -1211,8 +1211,12 @@ public class QueueImpl implements Queue
    public void destroyPaging() throws Exception
    {
       this.pageDestroyed = true;
-      pageSubscription.destroy();
-      pageSubscription.cleanupEntries(true);
+      // it could be null on embedded or certain unit tests
+      if (pageSubscription != null)
+      {
+         pageSubscription.destroy();
+         pageSubscription.cleanupEntries(true);
+      }
    }
 
    public synchronized boolean deleteReference(final long messageID) throws Exception
