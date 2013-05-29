2fc3ff758a132627df31bae94b90f0812bcfe8d4
John Ricklefs <jdricklefs@gmail.com>
2013-05-29 06:39:15 -0400

    HORNETQ-663 - Use explicit locks in TimeoutTask to prevent blocking creation of new subscriptions while culler is running.

--- hornetq-rest/src/test/java/org/hornetq/rest/test/AutoAckTopicTest.java
+++ hornetq-rest/src/test/java/org/hornetq/rest/test/AutoAckTopicTest.java
@@ -87,4 +87,134 @@ public class AutoAckTopicTest extends MessageTestBase
       Assert.assertEquals(204, sub1.request().delete().getStatus());
       Assert.assertEquals(204, sub2.request().delete().getStatus());
    }
+
+   @Test
+   public void testNewSubNotBlockedByTimeoutTask() throws Exception
+   {
+      // Default config is 1s interval, 300s timeout.
+
+      // Create a topic
+      String testName = "AutoAckTopicTest.testNewSubNotBlockedByTimeoutTask";
+      TopicDeployment deployment = new TopicDeployment();
+      deployment.setDuplicatesAllowed(true);
+      deployment.setDurableSend(false);
+      deployment.setName(testName);
+      manager.getTopicManager().deploy(deployment);
+
+      // Create a consumer
+      ClientRequest request = new ClientRequest(generateURL("/topics/" + testName));
+      ClientResponse<?> response = request.head();
+      response.releaseConnection();
+      Assert.assertEquals(200, response.getStatus());
+      Link sender = MessageTestBase.getLinkByTitle(manager.getTopicManager().getLinkStrategy(), response, "create");
+      Link subscriptions = MessageTestBase.getLinkByTitle(manager.getTopicManager().getLinkStrategy(), response, "pull-subscriptions");
+
+      // Create the pull-subscription itself.
+      ClientResponse<?> res = subscriptions.request().post();
+      res.releaseConnection();
+      Assert.assertEquals(201, res.getStatus());
+      Link sub1 = res.getLocation();
+      Assert.assertNotNull(sub1);
+      Link consumeNext1 = MessageTestBase.getLinkByTitle(manager.getTopicManager().getLinkStrategy(), res, "consume-next");
+      Assert.assertNotNull(consumeNext1);
+
+      // Pull on the topic for 8s (long enoguh to guarantee the rest of the test
+      // will pass/fail due to the timeouttask + test operations)
+      AcceptWaitListener awlistener = new AcceptWaitListener(consumeNext1.getHref());
+      Thread t = new Thread(awlistener);
+      t.start();
+      // Wait 2 seconds to ensure a new TimeoutTask is running concurrently.
+      Thread.sleep(2000);
+      // Attempt to create a new pull-subscription. Validate that it takes no longer than 2 seconds
+      // (it should take like 20ms, but give it a relatively huge amount of leeway)
+      NewPullSubscriber nps = new NewPullSubscriber(subscriptions.getHref());
+      Thread npsThread = new Thread(nps);
+      npsThread.start();
+      Thread.sleep(2000);
+      Assert.assertTrue("NewPullSubscriber did not finish in 2 seconds!", nps.isFinished());
+      Assert.assertFalse("AcceptWaitListener failed to open connection!", awlistener.isFailed());
+      Assert.assertFalse("NewPullSubscriber failed to open new subscription!", nps.isFailed());
+      npsThread.interrupt();
+      t.interrupt();
+   }
+
+   private class NewPullSubscriber implements Runnable
+   {
+      private final String url;
+      private boolean isFinished = false;
+      private boolean failed = false;
+
+      public NewPullSubscriber(String url) 
+      {
+         this.url = url;
+      }
+
+      public boolean isFinished()
+      {
+         return isFinished;
+      }
+
+      public boolean isFailed()
+      {
+         return failed;
+      }
+
+      public void run() 
+      {
+         try 
+         {
+            isFinished = false;
+            ClientRequest request = new ClientRequest(url);
+            ClientResponse<?> response = request.post();
+            response.releaseConnection();
+            System.out.println("NPS response: " + response.getStatus());
+            Assert.assertEquals(201, response.getStatus());
+            isFinished = true;
+         } 
+         catch(Exception e) 
+         {
+            System.out.println("Exception " + e);
+            failed = true;
+         }
+      }
+   }
+
+   private class AcceptWaitListener implements Runnable
+   {
+      private final int acceptWaitTime = 8;
+      private String url;      
+      private boolean isFinished = false;
+      private boolean failed = false;
+
+      public AcceptWaitListener(String url)
+      {
+            this.url = url;
+      }
+
+      public boolean isFinished()
+      {
+            return this.isFinished;
+      }
+
+      public boolean isFailed()
+      {
+            return this.failed;
+      }
+
+      public void run() 
+      {
+            try 
+            {
+                  ClientRequest req = new ClientRequest(url);
+                  req.header("Accept-Wait", acceptWaitTime);
+                  ClientResponse<?> response = req.post();
+                  response.releaseConnection();
+                  isFinished = true;
+            } 
+            catch(Exception e) 
+            {
+                  failed = true;
+            }
+      }
+   }
 }
