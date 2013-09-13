2ec356369a854f84d1d893003d7987ed18f2f012
clebert suconic <clebert.suconic@gmail.com>
2013-09-13 17:53:18 -0400

    https://issues.jboss.org/browse/HORNETQ-1254 - changing routing logic to only send locally when forwardWhenNoConsumers = false

--- hornetq-server/src/test/java/org/hornetq/tests/util/UnitTestCase.java
+++ hornetq-server/src/test/java/org/hornetq/tests/util/UnitTestCase.java
@@ -215,7 +215,7 @@ public abstract class UnitTestCase extends CoreUnitTestCase
                                                                                       List<String> connectors)
    {
       ClusterConnectionConfiguration ccc =
-               new ClusterConnectionConfiguration("cluster1", "jms", connectorName, 10, false, false, 1, 1, connectors,
+               new ClusterConnectionConfiguration("cluster1", "jms", connectorName, 10, false, true, 1, 1, connectors,
                                                   false);
       mainConfig.getClusterConfigurations().add(ccc);
    }
