573ae0239353a94afe695950d11e0039d17e79fb
clebert suconic <clebert.suconic@gmail.com>
2013-11-20 11:00:33 -0500

    https://bugzilla.redhat.com/show_bug.cgi?id=1032815 & https://issues.jboss.org/browse/HORNETQ-1284 - TokenBucket limiting per second now

--- tests/timing-tests/src/test/java/org/hornetq/tests/timing/util/TokenBucketLimiterImplTest.java
+++ tests/timing-tests/src/test/java/org/hornetq/tests/timing/util/TokenBucketLimiterImplTest.java
@@ -13,6 +13,12 @@
 
 package org.hornetq.tests.timing.util;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.hornetq.tests.util.RandomUtil;
+import org.hornetq.utils.Random;
 import org.junit.Test;
 
 import org.junit.Assert;
@@ -116,22 +122,157 @@ public class TokenBucketLimiterImplTest extends UnitTestCase
 
       final long measureTime = 5000;
 
+      // Do some initial testing ..   to ramp up the calculations
+      while (System.currentTimeMillis() - start < measureTime)
+      {
+         tbl.limit();
+      }
+
+      // wait some time
+      Thread.sleep(2000);
+
+
+      // start measuring again
+      start = System.currentTimeMillis();
+
       while (System.currentTimeMillis() - start < measureTime)
       {
          tbl.limit();
 
+         // when using a low rate (1 for instance), the very last could come after or very close to 5 seconds
+         // what could give us a false negative
          count++;
       }
 
       long end = System.currentTimeMillis();
 
-      double actualRate = (double)(1000 * count) / (end - start);
+      if (rate == 1)
+      {
+         // in 5 seconds you may get exactly 6 buckets..
+         // Count... 1, 2, 3, 4, 5, 6
+         // Time.... 0, 1, 2, 3, 4, 5
 
-      TokenBucketLimiterImplTest.log.debug("Desired rate: " + rate + " Actual rate " + actualRate + " invs/sec");
+         // any variation on 1 could make the test to fail, for that reason we make it this way
 
-      Assert.assertTrue(actualRate > rate * (1 - error));
+         Assert.assertTrue(count == 5 || count == 6);
+      }
+      else
+      {
+         double actualRate = (double)(1000 * count) / measureTime;
 
-      Assert.assertTrue(actualRate < rate * (1 + error));
+         Assert.assertTrue("actual rate = " + actualRate + " expected=" + rate, actualRate > rate * (1 - error));
+
+         Assert.assertTrue("actual rate = " + actualRate + " expected=" + rate, actualRate < rate * (1 + error));
+      }
+   }
+
+   @Test
+   public void testVerifyMaxRate1() throws Exception
+   {
+      testVerifyRate(1, 1, 5000);
+   }
+
+   @Test
+   public void testVerifyMaxRate5() throws Exception
+   {
+      testVerifyRate(5, 1, 5000);
+   }
+
+   @Test
+   public void testVerifyMaxRate50() throws Exception
+   {
+      testVerifyRate(50, 1, 5000);
+   }
+
+   @Test
+   public void testVerifyMaxRate50_per_5seconds() throws Exception
+   {
+      testVerifyRate(50, 5, 20000);
+   }
+
+
+   public void testVerifyRate(final int rate, final int window, final int timeRunning) throws Exception
+   {
+      final double error = 1.05; // Allow for 5% error
+
+      final AtomicBoolean running = new AtomicBoolean(true);
+
+      final AtomicBoolean rateError = new AtomicBoolean(false);
+
+      final AtomicInteger iterations = new AtomicInteger(0);
+
+      TokenBucketLimiterImpl tbl = new TokenBucketLimiterImpl(rate, false, TimeUnit.SECONDS, window);
+
+      Thread t = new Thread()
+      {
+         public void run()
+         {
+            int lastRun = 0;
+            long lastTime = System.currentTimeMillis();
+            while (running.get())
+            {
+               int tmpValue = iterations.get();
+               if (lastRun != 0)
+               {
+                  int consumed = tmpValue - lastRun;
+
+                  double calculatedRate = consumed  * window * 1000 / ((System.currentTimeMillis() - lastTime));
+
+                  if (calculatedRate > rate * error)
+                  {
+                     System.out.println("got more than " + rate + " tokens / second");
+                     rateError.set(true);
+                  }
+                  else if (calculatedRate > rate)
+                  {
+                     System.out.println("got more than " + rate + " tokens / second but still on the error marging" +
+                                        "make sure it's ok though, if you see to many of this message it's an issue");
+                  }
+                  System.out.println("Rate = " + calculatedRate + " consumed = " + consumed);
+               }
+               lastTime = System.currentTimeMillis();
+               lastRun = tmpValue;
+               try
+               {
+                  Thread.sleep(window * 1000);
+               }
+               catch (Exception e)
+               {
+                  e.printStackTrace();
+               }
+            }
+         }
+      };
+
+      t.start();
+
+
+      long timeout;
+
+      timeout = System.currentTimeMillis() + 3000;
+
+
+      while (timeout > System.currentTimeMillis())
+      {
+         tbl.limit();
+         iterations.incrementAndGet();
+      }
+
+      Thread.sleep(3000);
+
+      timeout = System.currentTimeMillis() + timeRunning;
+
+
+      while (timeout > System.currentTimeMillis())
+      {
+         tbl.limit();
+         iterations.incrementAndGet();
+      }
+
+
+      running.set(false);
+
+      t.join();
 
    }
 }
