d04c1666777d8cbc6db57ba3c14e78764a370765
Andy Taylor <ataylor@redhat.com>
2014-07-09 11:00:23 +0100

    https://bugzilla.redhat.com/show_bug.cgi?id=1115300 https://issues.jboss.org/browse/HORNETQ-1365 use waitforonmessage when the MDB is being interrupted

--- hornetq-ra/src/main/java/org/hornetq/ra/inflow/HornetQMessageHandler.java
+++ hornetq-ra/src/main/java/org/hornetq/ra/inflow/HornetQMessageHandler.java
@@ -32,6 +32,7 @@ import org.hornetq.core.client.impl.ClientSessionInternal;
 import org.hornetq.jms.client.HornetQDestination;
 import org.hornetq.jms.client.HornetQMessage;
 import org.hornetq.ra.HornetQRALogger;
+import org.hornetq.utils.FutureLatch;
 
 /**
  * The message handler
@@ -204,19 +205,20 @@ public class HornetQMessageHandler implements MessageHandler
       return useXA ? session : null;
    }
 
-   public void interruptConsumer()
+   public Thread interruptConsumer(FutureLatch future)
    {
       try
       {
          if (consumer != null)
          {
-            consumer.interruptHandlers();
+            return consumer.prepareForClose(future);
          }
       }
       catch (Throwable e)
       {
          HornetQRALogger.LOGGER.warn("Error interrupting handler on endpoint " + endpoint + " handler=" + consumer);
       }
+      return null;
    }
 
    /**
