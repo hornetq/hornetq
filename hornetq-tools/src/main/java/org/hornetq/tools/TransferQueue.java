3f48290aed2a3d0e372b57e41bb987ac64842504
clebert suconic <clebert.suconic@gmail.com>
2014-07-14 11:08:04 -0400

    adding filter to TransferQueue
    adding user/password / few tweaks on transfer tool

--- hornetq-tools/src/main/java/org/hornetq/tools/TransferQueue.java
+++ hornetq-tools/src/main/java/org/hornetq/tools/TransferQueue.java
@@ -38,7 +38,7 @@ public class TransferQueue // NO_UCD (unused code)
 
    public void process(String[] arg)
    {
-      if (arg.length != 9)
+      if (arg.length != 13 && arg.length != 14)
       {
          System.out.println("Invalid number of arguments! " + arg.length);
          printUsage();
@@ -46,29 +46,44 @@ public class TransferQueue // NO_UCD (unused code)
       }
 
       String sourceHost;
+      String sourceUser;
+      String sourcePass;
       int sourcePort;
       String queue;
 
       String targetHost;
       int targetPort;
+      String targetUser;
+      String targetPassword;
       String producingAddress;
 
       int commit;
 
       int waitTimeout;
 
+      String filter = null;
+
       try
       {
          sourceHost = arg[1];
          sourcePort = Integer.parseInt(arg[2]);
-         queue = arg[3];
+         sourceUser = arg[3];
+         sourcePass = arg[4];
+         queue = arg[5];
 
-         targetHost = arg[4];
-         targetPort = Integer.parseInt(arg[5]);
-         producingAddress = arg[6];
+         targetHost = arg[6];
+         targetPort = Integer.parseInt(arg[7]);
+         targetUser = arg[8];
+         targetPassword = arg[9];
+         producingAddress = arg[10];
 
-         waitTimeout = Integer.parseInt(arg[7]);
-         commit = Integer.parseInt(arg[8]);
+         waitTimeout = Integer.parseInt(arg[11]);
+         commit = Integer.parseInt(arg[12]);
+
+         if (arg.length == 14)
+         {
+            filter = arg[13];
+         }
       }
       catch (Exception e)
       {
@@ -95,18 +110,29 @@ public class TransferQueue // NO_UCD (unused code)
 
          ClientSessionFactory factorySource = locatorSource.createSessionFactory();
 
-         ClientSession sessionSource = factorySource.createSession(false, false);
+         ClientSession sessionSource = factorySource.createSession(sourceUser, sourcePass, false, false, false, false, 0);
 
-         ClientConsumer consumer = sessionSource.createConsumer(queue);
+         ClientConsumer consumer;
+
+         if (filter == null)
+         {
+            consumer = sessionSource.createConsumer(queue);
+         }
+         else
+         {
+            consumer = sessionSource.createConsumer(queue, filter);
+         }
 
 
          TransportConfiguration configurationTarget = new TransportConfiguration(NettyConnectorFactory.class.getName(), targetParameters);
 
-         ServerLocator locatorTarget = HornetQClient.createServerLocator(false, configurationSource);
+         ServerLocator locatorTarget = HornetQClient.createServerLocatorWithoutHA(configurationTarget);
 
-         ClientSessionFactory factoryTarget = locatorSource.createSessionFactory();
 
-         ClientSession sessionTarget = factorySource.createSession(false, false);
+
+         ClientSessionFactory factoryTarget = locatorTarget.createSessionFactory();
+
+         ClientSession sessionTarget = factoryTarget.createSession(targetUser, targetPassword, false, false, false, false, 0);
 
          ClientProducer producer = sessionTarget.createProducer(producingAddress);
 
@@ -123,6 +149,11 @@ public class TransferQueue // NO_UCD (unused code)
             }
 
             message.acknowledge();
+
+            if (!message.containsProperty("_HQ_TOOL_original_address"))
+            {
+               message.putStringProperty("_HQ_TOOL_original_address", message.getAddress().toString());
+            }
 
             LinkedList<String> listToRemove = new LinkedList<String>();
 
@@ -153,6 +184,8 @@ public class TransferQueue // NO_UCD (unused code)
 
          sessionTarget.commit();
          sessionSource.commit();
+         consumer.close();
+         producer.close();
 
          sessionSource.close();
          sessionTarget.close();
@@ -182,18 +215,26 @@ public class TransferQueue // NO_UCD (unused code)
       }
       System.err.println("This method will transfer messages from one queue into another, while removing internal properties such as ROUTE_TO.");
       System.err.println();
-      System.err.println(Main.USAGE + " <source-IP> <source-port> <source-queue> <target-IP> <target-port> <target-address> <wait-timeout> <commit-size>");
+      System.err.println(Main.USAGE + " <source-IP> <source-port> <user> <password> <source-queue> <target-IP> <target-port> <user> <password> <target-address> <wait-timeout> <commit-size> [filter]");
       System.err.println();
-      System.err.println("source-IP: IP for the originating server for the messages");
+      System.err.println("source-host: IP/hostname for the originating server for the messages");
       System.err.println("source-port: port for the originating server for the messages");
+      System.err.println("user: Username used to connect to the source");
+      System.err.println("password: Password used to connect to the source");
+      System.err.println("source-port: port for the originating server for the messages");
+
       System.err.println("source-queue: originating queue for the messages");
       System.err.println();
-      System.err.println("target-IP: IP for the destination server for the messages");
+      System.err.println("target-host: IP/hostname for the destination server for the messages");
       System.err.println("target-port: port for the destination server for the messages");
+      System.err.println("user: Username used to connect to the target");
+      System.err.println("password: Password used to connect to the target");
       System.err.println("target-address: address at the destination server");
       System.err.println();
       System.err.println("wait-timeout: time in milliseconds");
-      System.err.println("commit-size batch size for each transaction (in number of messages)");
+      System.err.println("commit-size: batch size for each transaction (in number of messages)");
+      System.err.println();
+      System.err.println("filter: You can optionally add a filter to the original queue");
       for (int i = 0; i < 10; i++)
       {
          System.err.println();
