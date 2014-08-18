a59bd179c0b5f2dae71646ecac8dd59b4919e42b
clebert suconic <clebert.suconic@gmail.com>
2014-08-18 17:36:01 -0400

    Improving Journal print-data output for bytes arrays properties and improving SNF cluster visualization

---
+++ hornetq-commons/src/main/java/org/hornetq/utils/ByteUtil.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2005-2014 Red Hat, Inc.
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.hornetq.utils;
+
+/**
+ * @author Clebert Suconic
+ */
+
+public class ByteUtil
+{
+
+   protected static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+   public static String bytesToHex(byte[] bytes, int groupSize)
+   {
+      char[] hexChars = new char[bytes.length * 2 + numberOfGroups(bytes, groupSize)];
+      int outPos = 0;
+      for (int j = 0; j < bytes.length; j++)
+      {
+         if (j > 0 && j % groupSize == 0)
+         {
+            hexChars[outPos++] = ' ';
+         }
+         int v = bytes[j] & 0xFF;
+         hexChars[outPos++] = hexArray[v >>> 4];
+         hexChars[outPos++] = hexArray[v & 0x0F];
+      }
+      return new String(hexChars);
+   }
+
+   private static int numberOfGroups(byte[] bytes, int groupSize)
+   {
+      int groups = bytes.length / groupSize;
+
+      if (bytes.length % groupSize == 0)
+      {
+         groups--;
+      }
+
+      return groups;
+   }
+
+
+}
