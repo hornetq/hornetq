b4eeb13fb15a3318a73a2b7acbd522e03fb7059e
clebert suconic <clebert.suconic@gmail.com>
2014-09-11 17:07:24 -0400

    HORNETQ-1407 - Wildcard matching for security-setting doesn't agree with documentation

--- hornetq-server/src/test/java/org/hornetq/core/settings/AddressSettingsTest.java
+++ hornetq-server/src/test/java/org/hornetq/core/settings/AddressSettingsTest.java
@@ -10,7 +10,7 @@
  * implied.  See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package org.hornetq.tests.unit.core.settings.impl;
+package org.hornetq.core.settings;
 
 import org.hornetq.api.core.SimpleString;
 import org.hornetq.core.settings.impl.AddressFullMessagePolicy;
