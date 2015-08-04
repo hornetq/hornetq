7c17c2d78933aa1a7c03a2a8ba8b03688317c7b2
Ryan Emerson <remerson@redhat.com>
2015-08-04 16:09:08 +0100

    [HORNETQ-1482] Add WARN log when setting connection-ttl OR connection-ttl-override equal to check-period.
    Now works when HornetQ is deployed in an application server as well as standalone.
    
    BZ-1175722: https://bugzilla.redhat.com/show_bug.cgi?id=1175722

--- hornetq-server/src/main/java/org/hornetq/core/config/ConfigurationUtils.java
+++ hornetq-server/src/main/java/org/hornetq/core/config/ConfigurationUtils.java
@@ -1,6 +1,7 @@
 package org.hornetq.core.config;
 
 import org.hornetq.api.core.HornetQIllegalStateException;
+import org.hornetq.core.server.HornetQServerLogger;
 
 public final class ConfigurationUtils
 {
@@ -22,5 +23,30 @@ public final class ConfigurationUtils
       }
       throw new HornetQIllegalStateException("Missing cluster-configuration for replication-cluster-name '" +
                                                 replicationCluster + "'.");
+   }
+
+   // A method to check the passed Configuration object and warn users if semantically unwise parameters are present
+   public static void validateConfiguration(Configuration configuration)
+   {
+      // Warn if connection-ttl-override/connection-ttl == check-period
+      compareTTLWithCheckPeriod(configuration);
+   }
+
+   private static void compareTTLWithCheckPeriod(Configuration configuration)
+   {
+      for (ClusterConnectionConfiguration c : configuration.getClusterConfigurations())
+         compareTTLWithCheckPeriod(c.getName(), c.getConnectionTTL(), configuration.getConnectionTTLOverride(), c.getClientFailureCheckPeriod());
+
+      for (BridgeConfiguration c : configuration.getBridgeConfigurations())
+         compareTTLWithCheckPeriod(c.getName(), c.getConnectionTTL(), configuration.getConnectionTTLOverride(), c.getClientFailureCheckPeriod());
+   }
+
+   private static void compareTTLWithCheckPeriod(String name, long connectionTTL, long connectionTTLOverride, long checkPeriod)
+   {
+      if (connectionTTLOverride == checkPeriod)
+         HornetQServerLogger.LOGGER.connectionTTLEqualsCheckPeriod(name, "connection-ttl-override", "check-period");
+
+      if (connectionTTL == checkPeriod)
+         HornetQServerLogger.LOGGER.connectionTTLEqualsCheckPeriod(name, "connection-ttl", "check-period");
    }
 }
