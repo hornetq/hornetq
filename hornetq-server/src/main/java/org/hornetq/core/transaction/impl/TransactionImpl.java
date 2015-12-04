72de644e07cf6ad8565dc24d1ec6b7b952e79da2
Tom Ross <Tom.Ross@redhat.com>
2015-12-04 15:11:18 +0000

    HORNETQ-1505 - fixing NullPointerException around Prepared Transactions.

--- hornetq-server/src/main/java/org/hornetq/core/transaction/impl/TransactionImpl.java
+++ hornetq-server/src/main/java/org/hornetq/core/transaction/impl/TransactionImpl.java
@@ -492,7 +492,14 @@ public class TransactionImpl implements Transaction
 
    public synchronized List<TransactionOperation> getAllOperations()
    {
-      return new ArrayList<TransactionOperation>(operations);
+      if ( operations != null)
+      {
+         return new ArrayList<TransactionOperation>(operations);
+      }
+      else
+      {
+         return new ArrayList<TransactionOperation>();
+      }
    }
 
    public void putProperty(final int index, final Object property)
