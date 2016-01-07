c26d43d7df43a539d311c2f2fec2b7b4fe36c1ee
Clebert Suconic <clebertsuconic@apache.org>
2016-01-07 11:13:15 -0500

    BZ-1296634 avoiding Shutdown from interrupted threads on IO
    https://bugzilla.redhat.com/show_bug.cgi?id=1296634
    (cherry picked from commit 496dbf1e86da0dd6bd884bf5fdbfab9937ef7c96)

--- tests/unit-tests/src/test/java/org/hornetq/tests/unit/core/paging/impl/PagingStoreImplTest.java
+++ tests/unit-tests/src/test/java/org/hornetq/tests/unit/core/paging/impl/PagingStoreImplTest.java
@@ -900,11 +900,6 @@ public class PagingStoreImplTest extends UnitTestCase
    static final class FakeStoreFactory implements PagingStoreFactory
    {
 
-      @Override
-      public void criticalException(Throwable e)
-      {
-      }
-
       final SequentialFileFactory factory;
 
       public FakeStoreFactory()
