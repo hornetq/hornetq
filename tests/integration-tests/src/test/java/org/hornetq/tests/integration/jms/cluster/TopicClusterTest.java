b87f418cc4a3e2a7665a9df87d8921e22a3e29b4
Howard Gao <howard.gao@gmail.com>
2017-06-26 13:00:35 +0800

    Bug1431406 specific properties not removed from messages
    In a cluster if a message is routed to other nodes an internal
    property (sth like _HQ_ROUTE_TOsfxxx) will be put to message
    in order to route the message to its remote destination.
    This property can be propogated to the consumers that
    doesn't recognise the property.
    
    For example if a 3rd party JMS consumer receives the message
    it can get the property name by JMS api	Message.getPropertyNames()
    and if the name is not confirm to some rules an erro will
    occur.
    
    The fix is that HorentQ message shouldn't expose such property
    via JMS API.

--- tests/integration-tests/src/test/java/org/hornetq/tests/integration/jms/cluster/TopicClusterTest.java
+++ tests/integration-tests/src/test/java/org/hornetq/tests/integration/jms/cluster/TopicClusterTest.java
@@ -15,16 +15,24 @@ package org.hornetq.tests.integration.jms.cluster;
 
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
+import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
 
+import org.hornetq.api.core.SimpleString;
+import org.hornetq.api.core.client.ClientMessage;
+import org.hornetq.core.message.impl.MessageImpl;
+import org.hornetq.jms.client.HornetQMessage;
 import org.hornetq.tests.util.JMSClusteredTestBase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Enumeration;
+import java.util.Set;
 
 /**
  * A TopicClusterTest
@@ -116,6 +124,97 @@ public class TopicClusterTest extends JMSClusteredTestBase
       jmsServer2.destroyTopic("t1");
 
 
+   }
+
+   @Test
+   public void testInternalPropertyNotExposed() throws Exception
+   {
+      Connection conn1 = cf1.createConnection();
+
+      conn1.setClientID("someClient1");
+
+      Connection conn2 = cf2.createConnection();
+
+      conn2.setClientID("someClient2");
+
+      conn1.start();
+
+      conn2.start();
+
+      try
+      {
+
+         Topic topic1 = createTopic("t1");
+
+         Session session1 = conn1.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         Session session2 = conn2.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+         MessageProducer prod1 = session1.createProducer(topic1);
+         prod1.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+         MessageConsumer cons1 = session1.createDurableSubscriber(topic1, "sub1");
+         MessageConsumer cons2 = session2.createDurableSubscriber(topic1, "sub2");
+
+         final int num = 1;
+         for (int i = 0; i < num; i++)
+         {
+            prod1.send(session1.createTextMessage("someMessage" + i));
+         }
+
+         for (int i = 0; i < num; i++)
+         {
+            TextMessage m1 = (TextMessage)cons1.receive(5000);
+            assertNotNull(m1);
+            TextMessage m2 = (TextMessage)cons2.receive(5000);
+            assertNotNull(m2);
+            checkInternalProperty(m1, m2);
+         }
+
+      }
+      finally
+      {
+         conn1.close();
+         conn2.close();
+
+         jmsServer1.destroyTopic("t1");
+         jmsServer2.destroyTopic("t1");
+      }
+   }
+
+   //check that the internal property is in the core
+   //but didn't exposed to jms
+   private void checkInternalProperty(Message... msgs) throws Exception
+   {
+      boolean checked = false;
+      for (Message m : msgs)
+      {
+         HornetQMessage hqMessage = (HornetQMessage) m;
+         ClientMessage coreMessage = hqMessage.getCoreMessage();
+         Set<SimpleString> coreProps = coreMessage.getPropertyNames();
+         System.out.println("core props: " + coreProps);
+         boolean exist = false;
+         for (SimpleString prop : coreProps)
+         {
+            if (prop.startsWith(MessageImpl.HDR_ROUTE_TO_IDS))
+            {
+               exist = true;
+               break;
+            }
+         }
+
+         if (exist)
+         {
+            Enumeration enumProps = m.getPropertyNames();
+            while (enumProps.hasMoreElements())
+            {
+               String propName = (String) enumProps.nextElement();
+               assertFalse("Shouldn't be in jms property: " + propName, propName.startsWith(MessageImpl.HDR_ROUTE_TO_IDS.toString()));
+            }
+            checked = true;
+         }
+      }
+      assertTrue(checked);
    }
 
    // Package protected ---------------------------------------------
